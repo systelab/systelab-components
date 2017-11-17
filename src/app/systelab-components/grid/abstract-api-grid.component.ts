8dadc5bf053a84b66fbfc5c30d6f4c8228c65d0a
alfonsserra <alfonsoserra@gmail.com>
2017-11-17 09:06:28 +0100

    Added Abstract Api Grid

---
+++ src/app/systelab-components/grid/abstract-api-grid.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { DialogService } from '../modal/dialog/dialog.service';
+import { AbstractGrid } from './abstract-grid.component';
+import { Observable } from 'rxjs/Observable';
+import { IDatasource, IGetRowsParams } from 'ag-grid';
+import { PreferencesService } from 'systelab-preferences/lib/preferences.service';
+import { I18nService } from 'systelab-translate/lib/i18n.service';
+
+export abstract class AbstractApiGrid<T> extends AbstractGrid<T> implements IDatasource, OnInit {
+
+	constructor(preferencesService: PreferencesService,
+				i18nService: I18nService,
+				dialogService: DialogService) {
+		super(preferencesService, i18nService, dialogService);
+	}
+
+	public ngOnInit() {
+
+		super.ngOnInit();
+
+		this.gridOptions.rowModelType = 'infinite';
+		this.gridOptions.paginationPageSize = 50;
+		this.gridOptions.cacheBlockSize = 50;
+		this.gridOptions.cacheOverflowSize = 2;
+		this.gridOptions.maxConcurrentDatasourceRequests = 4;
+		this.gridOptions.maxBlocksInCache = 15;
+		// this.gridOptions.paginationInitialRowCount = 0;
+		this.gridOptions.infiniteInitialRowCount = 0;
+
+		this.gridOptions.datasource = this;
+
+	}
+
+	protected abstract getTotalItems(): number;
+
+	protected abstract getData(page: number, itemsPerPage: number): Observable<Array<T>>;
+
+	public getRows(params: IGetRowsParams): void {
+
+		this.gridOptions.api.showLoadingOverlay();
+		this.getData(params.endRow / this.gridOptions.paginationPageSize, this.gridOptions.paginationPageSize)
+			.subscribe(
+				(v: Array<T>) => {
+					this.gridOptions.api.hideOverlay();
+					params.successCallback(v, this.getTotalItems());
+					if (this.forcedIndexSelection) {
+						this.gridOptions.api.selectIndex(this.forcedIndexSelection, false, false);
+						this.forcedIndexSelection = undefined;
+					}
+					/*
+					 if (!this.firstSizeToFitExecuted) {
+					 this.gridOptions.api.sizeColumnsToFit();
+					 this.firstSizeToFitExecuted = true;
+					 }
+					 */
+				},
+				error => {
+					this.gridOptions.api.hideOverlay();
+					params.successCallback([], 0);
+					/*
+					 if (!this.firstSizeToFitExecuted) {
+					 this.gridOptions.api.sizeColumnsToFit();
+					 this.firstSizeToFitExecuted = true;
+					 }
+					 */
+				}
+			);
+
+	}
+
+	public refresh() {
+		this.gridOptions.api.setDatasource(this);
+	}
+
+}
