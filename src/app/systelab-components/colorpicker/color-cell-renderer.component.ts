1415c0914030681e3af6a754a4cf53f0e2613040
alfonsserra <alfonsoserra@gmail.com>
2017-11-20 20:16:18 +0100

    Change selector names to start with systelab

--- src/app/systelab-components/colorpicker/color-cell-renderer.component.ts
+++ src/app/systelab-components/colorpicker/color-cell-renderer.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AgRendererComponent } from 'ag-grid-angular';
 
 @Component({
-	selector: 'mp-color-cell',
+	selector: 'systelab-colorpicker',
 	template: `<div class="color-tag" [ngStyle]="{'background-color': params.data.color, 'border-color': params.data.border}"></div>`
 })
 export class ColorCellRendererComponent implements AgRendererComponent {
