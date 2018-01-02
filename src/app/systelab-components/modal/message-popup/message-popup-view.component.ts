a55fd7dc96c72de75d594d8fc1ebf11a37540fc3
alfonsserra <alfonsoserra@gmail.com>
2018-01-02 16:50:19 +0100

    Remove DefaultModalActions

--- src/app/systelab-components/modal/message-popup/message-popup-view.component.ts
+++ src/app/systelab-components/modal/message-popup/message-popup-view.component.ts
@@ -10,27 +10,21 @@ export class MessagePopupViewContext extends ModulabModalContext {
 	public yesNoButtons: boolean;
 }
 
-class DefaultModalActions {
-
-	constructor(public dialog: DialogRef<ModulabModalContext>) {
-	}
-
-	public close(value?: any): void {
-		this.dialog.close(value);
-	}
-}
 
 @Component({
 	selector:    'dialog-view',
 	templateUrl: 'message-popup-view.component.html',
 	styleUrls:   ['message-popup-view.component.scss']
 })
-export class MessagePopupViewComponent extends DefaultModalActions implements ModalComponent<MessagePopupViewContext> {
+export class MessagePopupViewComponent implements ModalComponent<MessagePopupViewContext> {
 
 	public context: MessagePopupViewContext;
 
 	constructor(public dialog: DialogRef<MessagePopupViewContext>) {
-		super(dialog);
 		this.context = dialog.context;
+	}
+
+	public close(value?: any): void {
+		this.dialog.close(value);
 	}
 }
