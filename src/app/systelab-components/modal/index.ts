3b6b2fb0cdb2cd3d3499ee2a08cb21e0e88742bc
alfonsserra <alfonsoserra@gmail.com>
2018-01-02 17:04:21 +0100

    Export DialogService, MessagePopupService, SystelabModalContext in a index.ts

---
+++ src/app/systelab-components/modal/index.ts
@@ -0,0 +1,4 @@
+export { SystelabModalContext } from './modal-context';
+export { DialogService } from './dialog/dialog.service';
+export { MessagePopupService } from './message-popup/message-popup.service';
+
