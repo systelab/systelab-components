866d9b3e6030bf5c7c618b197e4b44037098c71d
alfonsserra <alfonsoserra@gmail.com>
2018-01-29 18:49:34 +0100

    Use i18nService for getFirstDayOfWeek and getDateFormat

--- src/app/systelab-components/calendar/calendar-table.component.ts
+++ src/app/systelab-components/calendar/calendar-table.component.ts
@@ -123,187 +123,64 @@ export class CalendarTableComponent implements OnChanges {
 	private getLanguage(): void {
 		this.language = {
 			dayNames:        [
-				this.i18nService.instant( 'COMMON_SUNDAY' ),
-				this.i18nService.instant( 'COMMON_MONDAY' ),
-				this.i18nService.instant( 'COMMON_TUESDAY' ),
-				this.i18nService.instant( 'COMMON_WEDNESDAY' ),
-				this.i18nService.instant( 'COMMON_THURSDAY' ),
-				this.i18nService.instant( 'COMMON_FRIDAY' ),
-				this.i18nService.instant( 'COMMON_SATURDAY' )
+				this.i18nService.instant('COMMON_SUNDAY'),
+				this.i18nService.instant('COMMON_MONDAY'),
+				this.i18nService.instant('COMMON_TUESDAY'),
+				this.i18nService.instant('COMMON_WEDNESDAY'),
+				this.i18nService.instant('COMMON_THURSDAY'),
+				this.i18nService.instant('COMMON_FRIDAY'),
+				this.i18nService.instant('COMMON_SATURDAY')
 			],
 			dayNamesShort:   [
-				this.i18nService.instant( 'COMMON_SEVENTH_DAY' ),
-				this.i18nService.instant( 'COMMON_FIRST_DAY' ),
-				this.i18nService.instant( 'COMMON_SECOND_DAY' ),
-				this.i18nService.instant( 'COMMON_THIRD_DAY' ),
-				this.i18nService.instant( 'COMMON_FOURTH_DAY' ),
-				this.i18nService.instant( 'COMMON_FIFTH_DAY' ),
-				this.i18nService.instant( 'COMMON_SIXTH_DAY' )
+				this.i18nService.instant('COMMON_SEVENTH_DAY'),
+				this.i18nService.instant('COMMON_FIRST_DAY'),
+				this.i18nService.instant('COMMON_SECOND_DAY'),
+				this.i18nService.instant('COMMON_THIRD_DAY'),
+				this.i18nService.instant('COMMON_FOURTH_DAY'),
+				this.i18nService.instant('COMMON_FIFTH_DAY'),
+				this.i18nService.instant('COMMON_SIXTH_DAY')
 			],
 			dayNamesMin:     [
-				this.i18nService.instant( 'COMMON_SEVENTH_DAY' ),
-				this.i18nService.instant( 'COMMON_FIRST_DAY' ),
-				this.i18nService.instant( 'COMMON_SECOND_DAY' ),
-				this.i18nService.instant( 'COMMON_THIRD_DAY' ),
-				this.i18nService.instant( 'COMMON_FOURTH_DAY' ),
-				this.i18nService.instant( 'COMMON_FIFTH_DAY' ),
-				this.i18nService.instant( 'COMMON_SIXTH_DAY' )
+				this.i18nService.instant('COMMON_SEVENTH_DAY'),
+				this.i18nService.instant('COMMON_FIRST_DAY'),
+				this.i18nService.instant('COMMON_SECOND_DAY'),
+				this.i18nService.instant('COMMON_THIRD_DAY'),
+				this.i18nService.instant('COMMON_FOURTH_DAY'),
+				this.i18nService.instant('COMMON_FIFTH_DAY'),
+				this.i18nService.instant('COMMON_SIXTH_DAY')
 			],
 			monthNames:      [
-				this.i18nService.instant( 'COMMON_JANUARY' ),
-				this.i18nService.instant( 'COMMON_FEBRUARY' ),
-				this.i18nService.instant( 'COMMON_MARCH' ),
-				this.i18nService.instant( 'COMMON_APRIL' ),
-				this.i18nService.instant( 'COMMON_MAY' ),
-				this.i18nService.instant( 'COMMON_JUNE' ),
-				this.i18nService.instant( 'COMMON_JULY' ),
-				this.i18nService.instant( 'COMMON_AUGUST' ),
-				this.i18nService.instant( 'COMMON_SEPTEMBER' ),
-				this.i18nService.instant( 'COMMON_OCTOBER' ),
-				this.i18nService.instant( 'COMMON_NOVEMBER' ),
-				this.i18nService.instant( 'COMMON_DECEMBER' )
+				this.i18nService.instant('COMMON_JANUARY'),
+				this.i18nService.instant('COMMON_FEBRUARY'),
+				this.i18nService.instant('COMMON_MARCH'),
+				this.i18nService.instant('COMMON_APRIL'),
+				this.i18nService.instant('COMMON_MAY'),
+				this.i18nService.instant('COMMON_JUNE'),
+				this.i18nService.instant('COMMON_JULY'),
+				this.i18nService.instant('COMMON_AUGUST'),
+				this.i18nService.instant('COMMON_SEPTEMBER'),
+				this.i18nService.instant('COMMON_OCTOBER'),
+				this.i18nService.instant('COMMON_NOVEMBER'),
+				this.i18nService.instant('COMMON_DECEMBER')
 			],
 			monthNamesShort: [
-				this.i18nService.instant( 'JOB_MONTHS_1' ),
-				this.i18nService.instant( 'JOB_MONTHS_2' ),
-				this.i18nService.instant( 'JOB_MONTHS_3' ),
-				this.i18nService.instant( 'JOB_MONTHS_4' ),
-				this.i18nService.instant( 'JOB_MONTHS_5' ),
-				this.i18nService.instant( 'JOB_MONTHS_6' ),
-				this.i18nService.instant( 'JOB_MONTHS_7' ),
-				this.i18nService.instant( 'JOB_MONTHS_8' ),
-				this.i18nService.instant( 'JOB_MONTHS_9' ),
-				this.i18nService.instant( 'JOB_MONTHS_10' ),
-				this.i18nService.instant( 'JOB_MONTHS_11' ),
-				this.i18nService.instant( 'JOB_MONTHS_12' )
+				this.i18nService.instant('JOB_MONTHS_1'),
+				this.i18nService.instant('JOB_MONTHS_2'),
+				this.i18nService.instant('JOB_MONTHS_3'),
+				this.i18nService.instant('JOB_MONTHS_4'),
+				this.i18nService.instant('JOB_MONTHS_5'),
+				this.i18nService.instant('JOB_MONTHS_6'),
+				this.i18nService.instant('JOB_MONTHS_7'),
+				this.i18nService.instant('JOB_MONTHS_8'),
+				this.i18nService.instant('JOB_MONTHS_9'),
+				this.i18nService.instant('JOB_MONTHS_10'),
+				this.i18nService.instant('JOB_MONTHS_11'),
+				this.i18nService.instant('JOB_MONTHS_12')
 			]
 		};
 
-		this.language.firstDayOfWeek = this.getFirstDayOfWeek();
-		this.language.dateFormatValue = this.getDateFormat( true );
+		this.language.firstDayOfWeek = this.i18nService.getFirstDayOfWeek();
+		this.language.dateFormatValue = this.i18nService.getDateFormat(true);
 	}
-
-
-	private getFirstDayOfWeek(): number {
-		switch ( this.i18nService.getCurrentLanguage() ) {
-			case 'us': // 'US'
-			case 'en_US':
-			case 'zh': // 'CN'
-			case 'th': // 'TH'
-			case 'ja': // 'JP'
-				return 0;
-			case 'en': // 'GB'
-			case 'it': // 'IT'
-			case 'ar':
-			case 'es':
-			case 'bo':
-			case 'cl':
-			case 'co':
-			case 'cr':
-			case 'do':
-			case 'ec':
-			case 'gt':
-			case 'hn':
-			case 'mx':
-			case 'ni':
-			case 'pa':
-			case 'pe':
-			case 'pr':
-			case 'py':
-			case 'sv':
-			case 'ur':
-			case 've':
-			case 'fr':
-			case 'gl':
-			case 'ca':
-			case 'pl': // 'PL'
-			case 'lt': // 'LT'
-			case 'pt': // 'PT'
-			case 'pt_BR':
-			case 'nl': // 'NL'
-			case 'sk': // 'SK'
-			case 'ru': // 'RU'
-			case 'de': // 'DE'
-			case 'ko':
-			default:
-				return 1;
-		}
-	}
-
-	private getDateFormat( isFullYear: boolean ): string {
-		let stringDateFormat = '';
-		switch ( this.i18nService.getCurrentLanguage() ) {
-			case 'us':
-			case 'en_US':
-				stringDateFormat = 'm/d/y';
-				break;
-			case 'en':
-			case 'it':
-			case 'ar':
-			case 'es':
-			case 'bo':
-			case 'cl':
-			case 'co':
-			case 'cr':
-			case 'do':
-			case 'ec':
-			case 'gt':
-			case 'hn':
-			case 'mx':
-			case 'ni':
-			case 'pa':
-			case 'pe':
-			case 'pr':
-			case 'py':
-			case 'sv':
-			case 'ur':
-			case 've':
-			case 'fr':
-			case 'gl':
-			case 'ca':
-				stringDateFormat = 'dd/mm/y';
-				break;
-			case 'ko':
-				stringDateFormat = 'y. m. d';
-				break;
-			case 'pl':
-			case 'lt':
-				stringDateFormat = 'y-mm-dd';
-				break;
-			case 'pt':
-			case 'pt-BR':
-			case 'nl':
-				stringDateFormat = 'dd-mm-y';
-				break;
-			case 'sk':
-			case 'ru':
-				stringDateFormat = 'd.m.y';
-				break;
-			case 'zh':
-				stringDateFormat = 'y-m-d';
-				break;
-			case 'de':
-				stringDateFormat = 'dd.mm.y';
-				break;
-			case 'th':
-				stringDateFormat = 'd/m/y';
-				break;
-			case 'ja':
-				stringDateFormat = 'y/mm/dd';
-				break;
-			default:
-				stringDateFormat = 'dd/mm/y';
-				break;
-		}
-		if ( isFullYear ) {
-			stringDateFormat = stringDateFormat.replace( 'y', 'yy' );
-			if ( this.i18nService.getCurrentLanguage() === 'us' || this.i18nService.getCurrentLanguage() === 'en_US' ) {
-				stringDateFormat = stringDateFormat.replace( 'm', 'mm' );
-				stringDateFormat = stringDateFormat.replace( 'd', 'dd' );
-			}
-		}
-		return stringDateFormat;
-	}
-
 
 }
