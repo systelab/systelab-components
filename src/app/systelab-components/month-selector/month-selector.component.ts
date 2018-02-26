1464418206fc84292f731f09790b976d84ae9049
alfonsserra <alfonsoserra@gmail.com>
2018-02-26 15:13:35 +0100

    Reformat.

--- src/app/systelab-components/month-selector/month-selector.component.ts
+++ src/app/systelab-components/month-selector/month-selector.component.ts
@@ -1,109 +1,124 @@
-import { Component, Input, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
+
 export class Month {
-  constructor(public month: number, public text: string, public year: number, public isActive: boolean) {
-  }
+	constructor(public month: number, public text: string, public year: number, public isActive: boolean) {
+	}
 }
+
 @Component({
-  selector: 'systelab-month-selector',
-  templateUrl: './month-selector.component.html'
+	selector:    'systelab-month-selector',
+	templateUrl: './month-selector.component.html'
 })
 export class MonthSelectorComponent implements AfterViewInit {
 
-  @Input() public maxDate: Date;
-  @Input() public minDate: Date;
-  @Input() public currentDate: Date;
-  @Input() public isDisabled: boolean;
+	@Input() public maxDate: Date;
+	@Input() public minDate: Date;
+	@Input() public currentDate: Date;
+	@Input() public isDisabled: boolean;
 
-  private _selectedMonth: Month;
-  @Input()
-  get selectedMonth(): Month {
-    return this._selectedMonth;
-  }
-  @Output() public selectedMonthChange = new EventEmitter();
+	private _selectedMonth: Month;
+	@Input()
+	get selectedMonth(): Month {
+		return this._selectedMonth;
+	}
 
-  set selectedMonth(value: Month) {
-    this._selectedMonth = value;
-    this.selectedMonthChange.emit(this._selectedMonth);
-  }
-  public months: Array<Month> = [];
-  public isOpened = false;
-  public monthNames = [];
+	@Output() public selectedMonthChange = new EventEmitter();
 
-  constructor(private i18nService: I18nService) {
-    let date;
-    if (this.currentDate) {
-      date = this.currentDate;
-    }
-    else {
-      date = new Date();
-    }
-    this.setMonthsNames(date.getFullYear());
-    this.selectedMonth = new Month(0, '', 0, false);
-  }
+	set selectedMonth(value: Month) {
+		this._selectedMonth = value;
+		this.selectedMonthChange.emit(this._selectedMonth);
+	}
 
-  public ngAfterViewInit(): void {
-    if (!this.currentDate) {
-      this.currentDate = new Date();
-    }
-    this.getMonths();
-  }
-  public setMonthsNames(year: number) {
-    this.monthNames = [];
-    for (let i = 0; i <= 11; i++) {
-      const month = this.i18nService.formatMonthAndYear(new Date(year, i, 1));
-      this.monthNames.push(month);
-    }
-  }
-  public onClicked(e) {
-    this.isOpened = true;
-  }
-  public hideSelector() {
-    this.isOpened = false;
-  }
-  public getMonths() {
-    this.months = [];
-    const month = this.currentDate.getMonth();
-    const year = this.currentDate.getFullYear();
-    for (let i = 0; i < this.monthNames.length; i++) {
-      const date = new Date(year, i + 1, 1, 0, 0, 0, 0);
-      if (this.checkDateIntoIntervals(date)) {
-        const isActive = (i === month);
-        const monthObj: Month = new Month(i, this.monthNames[i], year, isActive);
-        this.months.push(monthObj);
-        if (isActive) {
-          this.selectedMonth = monthObj;
-        }
-      }
-    }
-    console.log(this.months);
-  }
-  public changeYear(v: number) {
-    const date = new Date(this.currentDate.getFullYear() + (v), this.currentDate.getMonth(), this.currentDate.getDate());
-    if (this.checkDateIntoIntervals(date)) {
-      this.currentDate = date;
-      this.setMonthsNames(this.currentDate.getFullYear());
-      this.getMonths();
-    }
-  }
-  public checkDateIntoIntervals(date: Date) {
-    if (this.minDate && this.maxDate) {
-      if (date >= this.minDate && date <= this.maxDate) { return true; }
-      else { return false; }
-    }
-    else if (this.minDate) {
-      if (date >= this.minDate) { return true; }
-      else { return false; }
-    }
-    else if (this.maxDate) {
-      if (date <= this.maxDate) { return true; }
-      else { return false; }
-    }
-    else { return true; }
-  }
-  public onClickedMonth(month: Month) {
-    this.currentDate = new Date(month.year, month.month, 1);
-    this.hideSelector();
-    this.getMonths();
-  }
+	public months: Array<Month> = [];
+	public isOpened = false;
+	public monthNames = [];
+
+	constructor(private i18nService: I18nService) {
+		if (this.currentDate) {
+			this.setMonthsNames(this.currentDate.getFullYear());
+		} else {
+			this.setMonthsNames(new Date().getFullYear());
+		}
+		this.selectedMonth = new Month(0, '', 0, false);
+	}
+
+	public ngAfterViewInit(): void {
+		if (!this.currentDate) {
+			this.currentDate = new Date();
+		}
+		this.getMonths();
+	}
+
+	public setMonthsNames(year: number) {
+		this.monthNames = [];
+		for (let i = 0; i <= 11; i++) {
+			const month = this.i18nService.formatMonthAndYear(new Date(year, i, 1));
+			this.monthNames.push(month);
+		}
+	}
+
+	public onClicked(e) {
+		this.isOpened = true;
+	}
+
+	public hideSelector() {
+		this.isOpened = false;
+	}
+
+	public getMonths() {
+		this.months = [];
+		const month = this.currentDate.getMonth();
+		const year = this.currentDate.getFullYear();
+		for (let i = 0; i < this.monthNames.length; i++) {
+			const date = new Date(year, i + 1, 1, 0, 0, 0, 0);
+			if (this.checkDateIntoIntervals(date)) {
+				const isActive = (i === month);
+				const monthObj: Month = new Month(i, this.monthNames[i], year, isActive);
+				this.months.push(monthObj);
+				if (isActive) {
+					this.selectedMonth = monthObj;
+				}
+			}
+		}
+	}
+
+	public addYear(num: number) {
+		const date = new Date(this.currentDate.getFullYear() + (num), this.currentDate.getMonth(), this.currentDate.getDate());
+		if (this.checkDateIntoIntervals(date)) {
+			this.currentDate = date;
+			this.setMonthsNames(this.currentDate.getFullYear());
+			this.getMonths();
+		}
+	}
+
+	public checkDateIntoIntervals(date: Date) {
+		if (this.minDate && this.maxDate) {
+			if (date >= this.minDate && date <= this.maxDate) {
+				return true;
+			} else {
+				return false;
+			}
+		} else if (this.minDate) {
+			if (date >= this.minDate) {
+				return true;
+			} else {
+				return false;
+			}
+		} else if (this.maxDate) {
+			if (date <= this.maxDate) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return true;
+		}
+	}
+
+	public onClickedMonth(month: Month) {
+		this.currentDate = new Date(month.year, month.month, 1);
+		this.hideSelector();
+		this.getMonths();
+	}
 }
--- src/app/systelab-components/signature-canvas/signature-canvas.component.ts
+++ src/app/systelab-components/signature-canvas/signature-canvas.component.ts
@@ -1,134 +1,144 @@
-import { Component, ViewChild, ElementRef, Input, AfterViewInit, EventEmitter, Output } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
-class point {
-  constructor(public x: number, public y: number) {
-  }
+
+export class Point {
+	constructor(public x: number, public y: number) {
+	}
 }
+
 @Component({
-  selector: 'systelab-signature-canvas',
-  templateUrl: './signature-canvas.component.html'
+	selector:    'systelab-signature-canvas',
+	templateUrl: './signature-canvas.component.html'
 })
 export class SignatureCanvasComponent implements AfterViewInit {
 
-  constructor() { }
-  private _code: string;
-  private _signature: string;
+	private _code: string;
+	private _signature: string;
 
-  @ViewChild('signature') sign: ElementRef;
-  @ViewChild('img') img: ElementRef;
-  public downPath = '';
-  public href = '';
-  public emptyImage;
-  public verificationActive = false;
-  public isCanvasEmpty = true;
-  public cx: CanvasRenderingContext2D;
-  @Input() public width = 400;
-  @Input() public height: number;
-  @Input() public withCodeVerification: boolean;
-  @Input() public isDownloadable: boolean;
-  @Input() public isIncorrectCode: boolean;
+	@ViewChild('signature') sign: ElementRef;
+	@ViewChild('img') img: ElementRef;
 
-  @Input()
-  get signature(): string {
-    return this._signature;
-  }
-  @Output() public signatureChange = new EventEmitter();
-  set signature(value: string) {
-    this._signature = value;
-    this.signatureChange.emit(this._signature);
-  }
+	public downPath = '';
+	public href = '';
+	public verificationActive = false;
+	public isCanvasEmpty = true;
+	public cx: CanvasRenderingContext2D;
 
-  @Output() finish = new EventEmitter();
-  public dofinish() {
-    this.finish.emit();
-  }
+	@Input() public width = 400;
+	@Input() public height: number;
+	@Input() public withCodeVerification: boolean;
+	@Input() public isDownloadable: boolean;
+	@Input() public isIncorrectCode: boolean;
 
-  @Input()
-  get code(): string {
-    return this._code;
-  }
-  @Output() codeChange = new EventEmitter();
+	@Input()
+	get signature(): string {
+		return this._signature;
+	}
 
-  set code(value: string) {
-    this._code = value;
-    this.codeChange.emit(this._code);
-  }
+	@Output() public signatureChange = new EventEmitter();
 
-  public ngAfterViewInit() {
-    this.configureCanvas();
-  }
-  public configureCanvas() {
-    const canvas: HTMLCanvasElement = this.sign.nativeElement;
-    if (canvas) {
-      this.cx = canvas.getContext('2d');
-      canvas.width = this.width;
-      canvas.height = this.height;
-      this.cx.lineWidth = 4;
-      this.cx.lineCap = 'round';
-      this.cx.strokeStyle = '#000000';
-      this.captureEvents(canvas);
-    }
-  }
+	set signature(value: string) {
+		this._signature = value;
+		this.signatureChange.emit(this._signature);
+	}
 
-  public captureEvents(canvas: HTMLCanvasElement) {
-    Observable
-      .fromEvent(canvas, 'mousedown')
-      .switchMap((e) => {
-        return Observable
-          .fromEvent(canvas, 'mousemove')
-          .takeUntil(Observable.fromEvent(canvas, 'mouseup'))
-          .takeUntil(Observable.fromEvent(canvas, 'mouseleave'))
-          .pairwise();
-      })
-      .subscribe((res: [MouseEvent, MouseEvent]) => {
-        const rect = canvas.getBoundingClientRect();
-        const prevPos: point = new point(res[0].clientX - rect.left, res[0].clientY - rect.top);
-        const currentPos: point = new point(res[1].clientX - rect.left, res[1].clientY - rect.top);
-        this.draw(prevPos, currentPos);
-        if (this.isCanvasEmpty) {
-          this.isCanvasEmpty = false;
-        }
-      });
-  }
+	@Output() finish = new EventEmitter();
 
-  public draw(prevPos: point, currentPos: point) {
-    if (!this.cx) { return; }
-    this.cx.beginPath();
-    if (prevPos) {
-      this.cx.moveTo(prevPos.x, prevPos.y);
-      this.cx.lineTo(currentPos.x, currentPos.y);
-      this.cx.stroke();
-    }
-  }
+	public dofinish() {
+		this.finish.emit();
+	}
 
-  public downloadImage() {
-    const canvas: HTMLCanvasElement = this.sign.nativeElement;
-    const dataURL = canvas.toDataURL('image/png');
-    this.href = dataURL;
-    const date = new Date();
-    this.downPath = date + 'signature.png';
-  }
+	@Input()
+	get code(): string {
+		return this._code;
+	}
 
-  public cleanCanvas() {
-    this.isCanvasEmpty = true;
-    this.cx.clearRect(0, 0, this.width, this.height);
-  }
+	@Output() codeChange = new EventEmitter();
 
-  public finishDrawing() {
-    const canvas: HTMLCanvasElement = this.sign.nativeElement;
-    const dataURL = canvas.toDataURL('image/png');
-    this.signature = dataURL;
-    if (this.withCodeVerification) {
-      canvas.setAttribute('style', 'display:none;');
-      this.verificationActive = true;
-    }
-    else { this.finish.emit(); }
-  }
+	set code(value: string) {
+		this._code = value;
+		this.codeChange.emit(this._code);
+	}
 
-  public returnCanvas() {
-    const canvas: HTMLCanvasElement = this.sign.nativeElement;
-    canvas.setAttribute('style', 'display:block;');
-    this.verificationActive = false;
-  }
+	public ngAfterViewInit() {
+		this.configureCanvas();
+	}
+
+	public configureCanvas() {
+		const canvas: HTMLCanvasElement = this.sign.nativeElement;
+		if (canvas) {
+			this.cx = canvas.getContext('2d');
+			canvas.width = this.width;
+			canvas.height = this.height;
+			this.cx.lineWidth = 4;
+			this.cx.lineCap = 'round';
+			this.cx.strokeStyle = '#000000';
+			this.captureEvents(canvas);
+		}
+	}
+
+	public captureEvents(canvas: HTMLCanvasElement) {
+		Observable
+			.fromEvent(canvas, 'mousedown')
+			.switchMap((e) => {
+				return Observable
+					.fromEvent(canvas, 'mousemove')
+					.takeUntil(Observable.fromEvent(canvas, 'mouseup'))
+					.takeUntil(Observable.fromEvent(canvas, 'mouseleave'))
+					.pairwise();
+			})
+			.subscribe((res: [MouseEvent, MouseEvent]) => {
+				const rect = canvas.getBoundingClientRect();
+				const prevPos = new Point(res[0].clientX - rect.left, res[0].clientY - rect.top);
+				const currentPos = new Point(res[1].clientX - rect.left, res[1].clientY - rect.top);
+				this.draw(prevPos, currentPos);
+				if (this.isCanvasEmpty) {
+					this.isCanvasEmpty = false;
+				}
+			});
+	}
+
+	public draw(previousPoint: Point, currentPoint: Point) {
+		if (!this.cx) {
+			return;
+		}
+		this.cx.beginPath();
+		if (previousPoint) {
+			this.cx.moveTo(previousPoint.x, previousPoint.y);
+			this.cx.lineTo(currentPoint.x, currentPoint.y);
+			this.cx.stroke();
+		}
+	}
+
+	public downloadImage() {
+		const canvas: HTMLCanvasElement = this.sign.nativeElement;
+		const dataURL = canvas.toDataURL('image/png');
+		this.href = dataURL;
+		const date = new Date();
+		this.downPath = date + 'signature.png';
+	}
+
+	public cleanCanvas() {
+		this.isCanvasEmpty = true;
+		this.cx.clearRect(0, 0, this.width, this.height);
+	}
+
+	public finishDrawing() {
+		const canvas: HTMLCanvasElement = this.sign.nativeElement;
+		const dataURL = canvas.toDataURL('image/png');
+		this.signature = dataURL;
+		if (this.withCodeVerification) {
+			canvas.setAttribute('style', 'display:none;');
+			this.verificationActive = true;
+		} else {
+			this.finish.emit();
+		}
+	}
+
+	public returnCanvas() {
+		const canvas: HTMLCanvasElement = this.sign.nativeElement;
+		canvas.setAttribute('style', 'display:block;');
+		this.verificationActive = false;
+	}
 }
