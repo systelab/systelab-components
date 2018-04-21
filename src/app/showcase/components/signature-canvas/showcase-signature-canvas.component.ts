607004c2c3166d9250abf1e0da30c1056208e3cf
alfonsserra <alfonsoserra@gmail.com>
2018-04-21 19:10:28 +0200

    Add parameter to method

--- src/app/showcase/components/signature-canvas/showcase-signature-canvas.component.ts
+++ src/app/showcase/components/signature-canvas/showcase-signature-canvas.component.ts
@@ -1,20 +1,19 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'showcase-signature-canvas',
-  templateUrl: './showcase-signature-canvas.component.html'
+	selector:    'showcase-signature-canvas',
+	templateUrl: './showcase-signature-canvas.component.html'
 })
 export class ShowcaseSignatureCanvasComponent {
-  public withCodeVerification = true;
-  public height = 200;
-  public width = 500;
-  public isDownloadable = true;
-  public txVerification = 'Enter your password to finish the signature';
-  public code = '';
-  public signature;
-  public isIncorrectCode = false;
-  constructor() { }
-  public doFinish() {
-  }
+	public withCodeVerification = true;
+	public height = 200;
+	public width = 500;
+	public isDownloadable = true;
+	public code = '';
+	public signature;
+	public isIncorrectCode = false;
+
+	public doFinish(event: any) {
+	}
 
 }
