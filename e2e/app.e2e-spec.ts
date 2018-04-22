85f46e6dea4c930e75386f134caaadef7244a5f5
alfonsserra <alfonsoserra@gmail.com>
2018-04-22 22:03:37 +0200

    Simplify the test code

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,11 +1,4 @@
 import { ShowcasePage } from './app.po';
-import * as blueharvest from 'blue-harvest';
-import * as path from 'path';
-
-const GOLDEN_IMAGES = [1, 2, 3, 4, 5, 6, 7].map((i) => {
-	return path.join(__dirname, `snapshots/snapshots_${i}.png`);
-});
-
 
 describe('systelab-components: Showcase Snapshot Testing', () => {
 	let page: ShowcasePage;
@@ -16,50 +9,37 @@ describe('systelab-components: Showcase Snapshot Testing', () => {
 
 	it('Check the Form Components Tab', async() => {
 		await page.navigateTo();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[0]);
+		const result = await page.checkNavigationItem(0);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Modals Tab', async() => {
-		await page.getNavItem(1).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[1]);
+		const result = await page.checkNavigationItem(1);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Navigation Tab', async() => {
-		await page.getNavItem(2).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[2]);
+		const result = await page.checkNavigationItem(2);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Tables Tab', async() => {
-		await page.getNavItem(3).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[3]);
+		const result = await page.checkNavigationItem(3);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Utils Tab', async() => {
-		await page.getNavItem(4).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[4]);
+		const result = await page.checkNavigationItem(4);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Icons Tab', async() => {
-		await page.getNavItem(5).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[5]);
+		const result = await page.checkNavigationItem(5);
 		expect(result).toBeTruthy();
 	});
 
 	it('Check the Styles Tab', async() => {
-		await page.getNavItem(6).click();
-		let data =  await page.takeScreenshot();
-		let result = await blueharvest.compareScreenshot(data,  GOLDEN_IMAGES[6]);
+		const result = await page.checkNavigationItem(6);
 		expect(result).toBeTruthy();
 	});
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,6 @@
 import { browser, by, element } from 'protractor';
+import * as blueharvest from 'blue-harvest';
+import * as path from 'path';
 
 export class ShowcasePage {
 
@@ -10,11 +12,13 @@ export class ShowcasePage {
 		return browser.takeScreenshot();
 	}
 
-	public getTitle() {
-		return browser.getTitle();
-	}
-
 	public getNavItem(i: number) {
 		return element(by.id('nav-' + i));
+	}
+
+	public async checkNavigationItem(i: number) {
+		await this.getNavItem(i).click();
+		const data = await this.takeScreenshot();
+		return await blueharvest.compareScreenshot(data, path.join(__dirname, `snapshots/snapshots_${i + 1}.png`));
 	}
 }
