3770c1aad75bc1271e54be9cf0ea848ea3e7e769
alfonsserra <alfonsoserra@gmail.com>
2018-05-13 19:41:32 +0200

    Use single quote

--- protractor-update-snapshot.conf.js
+++ protractor-update-snapshot.conf.js
@@ -2,6 +2,6 @@ let config = require('./protractor.conf.js').config;
 
 // If UPDATE_GOLDENS is "1" or "true", blue-harvest will update the reference
 // screenshots instead of comparing against them.
-process.env['UPDATE_GOLDENS'] = "true";
+process.env['UPDATE_GOLDENS'] = 'true';
 
 exports.config = config;
