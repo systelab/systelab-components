780ad9fbc5dd34c714e00b2dd97a25bca2f2bf04
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2018-05-28 18:42:26 +0200

    Combos only set default value if no value is set by input

--- src/app/systelab-components/select/all-yes-no-combobox.component.ts
+++ src/app/systelab-components/select/all-yes-no-combobox.component.ts
@@ -1,39 +1,41 @@
 import { AbstractComboBox } from '../combobox/abstract-combobox.component';
-import {ChangeDetectorRef, Component, Renderer2} from '@angular/core';
+import { ChangeDetectorRef, Component, Renderer2 } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 
 class Element {
-	constructor(public id: string, public description: string) {
+	constructor( public id: string, public description: string ) {
 
 	}
 }
 
-@Component({
+@Component( {
 	selector:    'systelab-all-yes-no-select',
 	templateUrl: '../combobox/abstract-combobox.component.html'
-})
+} )
 
 export class AllYesNoSelect extends AbstractComboBox {
-	constructor(myRenderer: Renderer2, public chRef: ChangeDetectorRef, public i18nService: I18nService) {
-		super(myRenderer, chRef);
+	constructor( myRenderer: Renderer2, public chRef: ChangeDetectorRef, public i18nService: I18nService ) {
+		super( myRenderer, chRef );
 		this.values = new Array<Element>();
-		this.values.push(new Element('A', this.i18nService.instant('COMMON_ALL')));
-		this.values.push(new Element('Y', this.i18nService.instant('COMMON_YES')));
-		this.values.push(new Element('N', this.i18nService.instant('COMMON_NO')));
-		this._id = 'A';
-		this._description = this.i18nService.instant('COMMON_ALL');
+		this.values.push( new Element( 'A', this.i18nService.instant( 'COMMON_ALL' ) ) );
+		this.values.push( new Element( 'Y', this.i18nService.instant( 'COMMON_YES' ) ) );
+		this.values.push( new Element( 'N', this.i18nService.instant( 'COMMON_NO' ) ) );
+		if ( !this._id ) {
+			this._id = 'A';
+			this._description = this.i18nService.instant( 'COMMON_ALL' );
+		}
 	}
 
-	public afterSettingId(value: number | string) {
-		switch (value) {
+	public afterSettingId( value: number | string ) {
+		switch ( value ) {
 			case 'Y':
-				this.description = this.i18nService.instant('COMMON_YES');
+				this.description = this.i18nService.instant( 'COMMON_YES' );
 				break;
 			case 'N':
-				this.description = this.i18nService.instant('COMMON_NO');
+				this.description = this.i18nService.instant( 'COMMON_NO' );
 				break;
 			case 'A':
-				this.description = this.i18nService.instant('COMMON_ALL');
+				this.description = this.i18nService.instant( 'COMMON_ALL' );
 				break;
 			default:
 				break;
