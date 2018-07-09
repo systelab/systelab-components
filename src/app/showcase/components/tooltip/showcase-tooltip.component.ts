25654e329265ed7f960a7a0194350d619c1ee5ff
VictorDeBlas <aa147896325>
2018-07-09 11:10:47 +0200

    Improvements on tooltips

--- src/app/showcase/components/tooltip/showcase-tooltip.component.ts
+++ src/app/showcase/components/tooltip/showcase-tooltip.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-	selector:    'showcase-tooltip',
+	selector: 'showcase-tooltip',
 	templateUrl: 'showcase-tooltip.component.html'
 })
-export class ShowcaseTooltipComponent {
+export class ShowcaseTooltipComponent implements OnInit {
 
+	public tooltipText = 'Tooltip on top';
 
 	constructor() {
+	}
+
+	public ngOnInit() {
+		this.executeDelayed();
+	}
+
+	private executeDelayed(): void {
+		setTimeout(() => {
+			this.tooltipText = 'This is a text delayed';
+		}, 4000);
 	}
 }
