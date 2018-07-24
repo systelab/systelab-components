9c76c2c509bb09743299478287a08cb81856e534
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2018-07-24 12:19:29 +0200

    navbar improvements: Added activeColor, activeFontColor and borderColor to customize component

--- src/app/systelab-components/navbar/navbar.component.ts
+++ src/app/systelab-components/navbar/navbar.component.ts
@@ -1,26 +1,29 @@
 import { Component, Input } from '@angular/core';
 
 export class NavbarItem {
-	constructor(public id: number, public text: string, public image: string, public floatImage: boolean, public isSelected: boolean,
-		public isEnabled: boolean, public action: any, public target?: string, public url?: string) {
+	constructor( public id: number, public text: string, public image: string, public floatImage: boolean, public isSelected: boolean, public isEnabled: boolean,
+				 public action: any, public target?: string, public url?: string ) {
 	}
 }
 
-@Component({
-	selector: 'systelab-navbar',
+@Component( {
+	selector:    'systelab-navbar',
 	templateUrl: 'navbar.component.html',
-})
+} )
 export class NavbarComponent {
 
 	@Input() public items: NavbarItem[] = [];
 	@Input() public isVertical = false;
 	@Input() public backgroundColor;
 	@Input() public backgroundHoverColor: string;
+	@Input() public borderColor: string;
+	@Input() public activeColor = 'white';
+	@Input() public activeFontColor = 'black';
 	@Input() public fontColor = 'white';
 	@Input() public align = 'left';
 	@Input() public id: string;
 
-	public hovered:number;
+	public hovered: number;
 
 	constructor() {
 	}
