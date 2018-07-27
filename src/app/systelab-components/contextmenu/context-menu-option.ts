a87020c59cc766a8a68106c3623dd8b521968035
VictorDeBlas <aa147896325>
2018-07-27 13:39:22 +0200

    ContextMenu with icons refactor

--- src/app/systelab-components/contextmenu/context-menu-option.ts
+++ src/app/systelab-components/contextmenu/context-menu-option.ts
@@ -4,20 +4,6 @@ export type ContextMenuActionFunction = (data: ContextMenuActionData) => void;
 export type ContextMenuIsEnabledFunction = (elementId: string, actionId: string) => boolean;
 export type ContextMenuIsIconEnabledFunction = (elementId: string, actionId: string) => boolean;
 
-export class IconConfiguration {
-	public iconClass: string;
-	public backgroundColor: string;
-	public iconColor: string;
-	public isIconEnabled: ContextMenuIsIconEnabledFunction;
-
-	constructor(public nIconClass?: string, public nBackgroundColor?: string, public nIconColor?: string, public nIsIconEnabled?: ContextMenuIsIconEnabledFunction) {
-		this.iconClass = nIconClass;
-		this.backgroundColor = nBackgroundColor;
-		this.iconColor = nIconColor;
-		this.isIconEnabled = nIsIconEnabled;
-	}
-}
-
 export class ContextMenuOption {
 
 	constructor(public actionId: string,
@@ -25,7 +11,10 @@ export class ContextMenuOption {
 	            public action?: ContextMenuActionFunction,
 	            public isActionEnabled?: ContextMenuIsEnabledFunction,
 	            public isDivider?: boolean,
-	            public iconConfig?: IconConfiguration) {
+	            public iconClass?: string,
+	            public backgroundColor?: string,
+	            public iconColor?: string,
+	            public isIconEnabled?: ContextMenuIsIconEnabledFunction) {
 	}
 
 }
