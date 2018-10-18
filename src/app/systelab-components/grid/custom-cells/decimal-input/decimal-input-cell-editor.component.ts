c210e716f6fb8f478e36b681af91f060021b124f
VictorDeBlas <aa147896325>
2018-10-18 16:01:10 +0200

    Fix on DecimalInputCellEditor from 6.3.56, and reviewed with Carlos Viñola

--- src/app/systelab-components/grid/custom-cells/decimal-input/decimal-input-cell-editor.component.ts
+++ src/app/systelab-components/grid/custom-cells/decimal-input/decimal-input-cell-editor.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {AgEditorComponent} from 'ag-grid-angular';
-import {DecimalPipe} from '@angular/common';
 
 @Component({
 	selector: 'systelab-decimal-input-cell-editor',
@@ -13,20 +12,13 @@ export class DecimalInputCellEditorComponent implements AgEditorComponent {
 	public id: string;
 	public isEditable = true;
 
-	constructor(protected decimalFormat: DecimalPipe) {
-	}
-
 	public agInit(params: any): void {
 		this.params = params;
 		this.decimalValue = this.params.value;
-		if (this.params.value !== null && typeof(this.params.value) === 'string') {
-			this.decimalValue = parseInt(this.params.value.replace(',', ''), 10);
-		}
 	}
 
 	public getValue(): any {
-		const transformedValue = this.decimalFormat.transform(this.decimalValue, '1.1-5');
-		return transformedValue ? transformedValue.replace(/,/g, '') : null;
+		return this.decimalValue;
 	}
 
 	public refresh(params: any): boolean {
