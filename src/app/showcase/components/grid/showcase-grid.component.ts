b00b26aa84c644590bc18cc43dd8a9e5d5627317
alfonsserra <alfonsoserra@gmail.com>
2018-10-26 10:20:42 +0200

    Showcase: Add a Menu in the Table Header

--- src/app/showcase/components/grid/showcase-grid.component.ts
+++ src/app/showcase/components/grid/showcase-grid.component.ts
@@ -12,8 +12,7 @@ export class ShowcaseGridComponent {
 	constructor() {
 	}
 
-	public doSelect(compareProfileData: ShowcaseData): void {
-
+	public doSelect(showcaseData: ShowcaseData): void {
 	}
 
 	public getMenu(): Array<GridContextMenuOption<ShowcaseData>> {
@@ -22,22 +21,13 @@ export class ShowcaseGridComponent {
 			new GridContextMenuOption('action2', 'Action 2', (a) => this.doMenuAction(a)),
 			new GridContextMenuOption('', '', (a) => this.doMenuAction(a), null, true),
 			new GridContextMenuOption('action3', 'Action 3', (a) => this.doMenuAction(a)),
-			new GridContextMenuOption('action4', 'Action 4', (a) => this.doMenuAction(a), (a) => {
-				return false;
-			})
-
+			new GridContextMenuOption('action4', 'Action 4', (a) => this.doMenuAction(a), a => false)
 		];
 	}
 
 	public doMenuAction(action: GridContextMenuActionData<ShowcaseData>): void {
-		if (action.actionId === 'action1') {
-			console.log('action1');
-		} else if (action.actionId === 'action2') {
-			console.log('action2');
-		} else if (action.actionId === 'action3') {
-			console.log('action3');
-		} else if (action.actionId === 'action4') {
-			console.log('action4');
-		}
+		console.log('Here ' + action.actionId);
+		console.log('With ' + action.data);
+
 	}
 }
