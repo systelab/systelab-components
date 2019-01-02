2085f7b6610fe41765b21766351dd5efc7881a6c
alfonsserra <alfonsoserra@gmail.com>
2019-01-02 13:17:18 +0100

    Showcase for Date Range picker

--- src/app/showcase/components/context-panel/showcase-context-panel.component.ts
+++ src/app/showcase/components/context-panel/showcase-context-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ContextMenuActionData } from '../../../systelab-components/contextmenu/context-menu-action-data';
 import { TouchSpinValues } from '../../../systelab-components/spinner/touch.spin-values';
 
 @Component({
@@ -10,14 +9,47 @@ export class ShowcaseContextPanel implements OnInit {
 	public touchSpinValues1: TouchSpinValues;
 	public touchSpinValues2: TouchSpinValues;
 	public fromDate: Date = null;
-	public toDate: Date = null;
+	public toDate: Date = new Date();
+
+	public numericDateRadio = 'days';
 
 	constructor() {
-		this.touchSpinValues1 = new TouchSpinValues(0, 1, 1, 999);
-		this.touchSpinValues2 = new TouchSpinValues(0, 1, 1, 999);
+		this.touchSpinValues1 = new TouchSpinValues(30, 1, 365, 1);
+		this.touchSpinValues2 = new TouchSpinValues(30, 1, 1000, 1);
 	}
 
 	public ngOnInit() {
+		this.fromDate=new Date();
+		this.fromDate.setMonth(this.fromDate.getMonth() - 1);
+	}
+
+	public getInterval() {
+		if (this.numericDateRadio === 'calendar') {
+			if (this.fromDate && this.toDate) {
+				return this.formatDate(this.fromDate) + ' to ' + this.formatDate(this.toDate);
+			}
+		}
+		if (this.numericDateRadio === 'days') {
+			return 'Last ' + this.touchSpinValues1.value + ' days';
+		}
+		if (this.numericDateRadio === 'number') {
+			return 'Last ' + this.touchSpinValues2.value + ' points';
+		}
+		return '';
+	}
+
+	private formatDate(date: Date) {
+		const monthNames = [
+			'January', 'February', 'March',
+			'April', 'May', 'June', 'July',
+			'August', 'September', 'October',
+			'November', 'December'
+		];
+		const day = date.getDate();
+		const monthIndex = date.getMonth();
+		const year = date.getFullYear();
+
+		return day + ' ' + monthNames[monthIndex] + ' ' + year;
 	}
 
 }
