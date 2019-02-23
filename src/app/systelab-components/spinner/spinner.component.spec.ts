f69c8f3a6d93adfc011887bb1db865520dd2849b
alfonsserra <alfonsoserra@gmail.com>
2019-02-23 22:42:32 +0100

    Set as 8.1.1

---
+++ src/app/systelab-components/spinner/spinner.component.spec.ts
@@ -0,0 +1,121 @@
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TouchSpinValues } from './touch.spin-values';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { TreeModule } from 'primeng/tree';
+import { HttpClientModule } from '@angular/common/http';
+import { TouchspinComponent } from './spinner.component';
+
+@Component({
+	selector: 'systelab-spinner-test',
+	template: `
+                <div>
+                    <systelab-spinner [spinValues]="values"></systelab-spinner>
+                    <label class="label-value">{{values.value}}</label>
+                </div>
+	          `,
+	styles:   []
+})
+export class SpinnerTestComponent {
+	public values = new TouchSpinValues(34, 1, 100);
+}
+
+describe('Systelab Spinner', () => {
+	let spinnerTestFixture: ComponentFixture<SpinnerTestComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports:      [BrowserModule,
+				BrowserAnimationsModule,
+				FormsModule,
+				DragDropModule,
+				OverlayModule,
+				TreeModule,
+				HttpClientModule],
+			declarations: [TouchspinComponent, SpinnerTestComponent]
+		}).compileComponents();
+	}));
+
+	beforeEach(() => {
+		spinnerTestFixture = TestBed.createComponent(SpinnerTestComponent);
+		spinnerTestFixture.detectChanges();
+	});
+
+	it('should instantiate', () => {
+		expect(spinnerTestFixture.componentInstance).toBeDefined();
+	});
+
+	it('should have an initial value', () => {
+		checkHasValue(spinnerTestFixture, 34);
+	});
+
+	it('should have the changed value if there is a change', () => {
+		setValue(spinnerTestFixture, 90);
+		checkHasValue(spinnerTestFixture, 90);
+	});
+
+	it('should increment the value if plus button is clicked (+1)', () => {
+		clickPlusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 34 + 1);
+	});
+
+	it('should increment the value n times if plus button is clicked (+11)', () => {
+		setStep(spinnerTestFixture, 11);
+		clickPlusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 34 + 11);
+	});
+
+	it('should not increment the value if is the maximum', () => {
+		setValue(spinnerTestFixture, 100);
+		clickPlusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 100);
+	});
+
+	it('should decrement the value if minus button is clicked (-1)', () => {
+		clickMinusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 34 - 1);
+	});
+
+	it('should decrement the value n times if minus button is clicked (-11)', () => {
+		setStep(spinnerTestFixture, 11);
+		clickMinusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 34 - 11);
+	});
+
+	it('should not decrement the value if is the minimum', () => {
+		setValue(spinnerTestFixture, 1);
+		clickMinusButton(spinnerTestFixture);
+		checkHasValue(spinnerTestFixture, 1);
+	});
+});
+
+function clickPlusButton(spinnerTestFixture: ComponentFixture<SpinnerTestComponent>) {
+	const button = spinnerTestFixture.debugElement.nativeElement.querySelector('#plus-button');
+	button.click();
+	spinnerTestFixture.detectChanges();
+}
+
+function clickMinusButton(spinnerTestFixture: ComponentFixture<SpinnerTestComponent>) {
+	const button = spinnerTestFixture.debugElement.nativeElement.querySelector('#minus-button');
+	button.click();
+	spinnerTestFixture.detectChanges();
+}
+
+function checkHasValue(spinnerTestFixture: ComponentFixture<SpinnerTestComponent>, value: number) {
+	const label = spinnerTestFixture.debugElement.nativeElement.querySelector('.label-value');
+	expect(label.innerHTML).toContain(value);
+}
+
+function setValue(spinnerTestFixture: ComponentFixture<SpinnerTestComponent>, value: number) {
+	spinnerTestFixture.componentInstance.values.value = value;
+	spinnerTestFixture.detectChanges();
+}
+
+function setStep(spinnerTestFixture: ComponentFixture<SpinnerTestComponent>, value: number) {
+	spinnerTestFixture.componentInstance.values.step = value;
+	spinnerTestFixture.detectChanges();
+}
