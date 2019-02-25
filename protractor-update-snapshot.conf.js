6af07750912f918e63c0e07770f8bfb2f4bf471b
alfonsserra <alfonsoserra@gmail.com>
2019-02-25 10:39:50 +0100

    In the dependency is UPDATE_GOLDEN_IMAGES instead of UPDATE_GOLDENS

--- protractor-update-snapshot.conf.js
+++ protractor-update-snapshot.conf.js
@@ -1,7 +1,7 @@
 let config = require('./protractor.conf.js').config;
 
-// If UPDATE_GOLDENS is "1" or "true", blue-harvest will update the reference
+// If UPDATE_GOLDEN_IMAGES is "1" or "true", blue-harvest will update the reference
 // screenshots instead of comparing against them.
-process.env['UPDATE_GOLDENS'] = 'true';
+process.env['UPDATE_GOLDEN_IMAGES'] = 'true';
 
 exports.config = config;
