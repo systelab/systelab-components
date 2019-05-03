53c32d510949eb4b5a475487f41141d9c3cc326b
David Gómez <david.gomez.fdez@gmail.com>
2019-05-03 12:25:15 +0200

    Changed AfterViewInit by OnInit mode in implementation of Gender combobox and All-yes-no in order to fix the ExpressionChangedAfterItHasBeenCheckedError after init the component. Also has been extracted the translation of different values of combobox to retrieve all the values of the combo in the same way

--- src/app/systelab-components/select/all-yes-no-combobox.component.ts
+++ src/app/systelab-components/select/all-yes-no-combobox.component.ts
@@ -1,6 +1,6 @@
-import { AbstractComboBox } from '../combobox/abstract-combobox.component';
-import { AfterViewInit, ChangeDetectorRef, Component, Renderer2 } from '@angular/core';
-import { I18nService } from 'systelab-translate/lib/i18n.service';
+import {AbstractComboBox} from '../combobox/abstract-combobox.component';
+import {ChangeDetectorRef, Component, OnInit, Renderer2} from '@angular/core';
+import {I18nService} from 'systelab-translate/lib/i18n.service';
 
 class Element {
 	constructor(public id: string, public description: string) {
@@ -12,22 +12,26 @@ class Element {
 	templateUrl: '../combobox/abstract-combobox.component.html'
 })
 
-export class AllYesNoSelect extends AbstractComboBox<Element> implements AfterViewInit {
+export class AllYesNoSelect extends AbstractComboBox<Element> implements OnInit {
 
 	private readonly descriptionAll: string;
+	private readonly descriptionYes: string;
+	private readonly descriptionNo: string;
 
 	constructor(myRenderer: Renderer2, public chRef: ChangeDetectorRef, public i18nService: I18nService) {
 		super(myRenderer, chRef);
 		this.descriptionAll = this.i18nService.instant('COMMON_ALL');
+		this.descriptionYes = this.i18nService.instant('COMMON_YES');
+		this.descriptionNo = this.i18nService.instant('COMMON_NO');
 	}
 
-	public ngAfterViewInit(): void {
+	public ngOnInit(): void {
 		const elements = new Array<Element>();
 		this.defaultDescription = this.descriptionAll;
 		this.defaultIdValue = '';
 		elements.push(new Element('', this.descriptionAll));
-		elements.push(new Element('Y', this.i18nService.instant('COMMON_YES')));
-		elements.push(new Element('N', this.i18nService.instant('COMMON_NO')));
+		elements.push(new Element('Y', this.descriptionYes));
+		elements.push(new Element('N', this.descriptionNo));
 		if (!this._id) {
 			this._id = '';
 		}
