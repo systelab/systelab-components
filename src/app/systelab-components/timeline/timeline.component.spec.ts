211359cc9c26169e369d7b1aeb0b98e3c5e8f19b
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2019-06-12 14:31:36 +0200

    systelab-timeline: Add click output + Minor improvements #238 systelab-tooltip: Error emptying the tooltip #239

--- src/app/systelab-components/timeline/timeline.component.spec.ts
+++ src/app/systelab-components/timeline/timeline.component.spec.ts
@@ -10,10 +10,10 @@ import { SystelabTranslateModule } from 'systelab-translate';
 @Component({
 	selector: 'systelab-timeline-test',
 	template: `
-                <div>
-                    <systelab-timeline [events]="events"></systelab-timeline>
-                </div>
-	          `,
+                  <div>
+                      <systelab-timeline [events]="events"></systelab-timeline>
+                  </div>
+			  `,
 	styles:   []
 })
 export class TimeLineTestComponent {
@@ -21,10 +21,22 @@ export class TimeLineTestComponent {
 	public events: TimelineEvent[] = [];
 
 	constructor() {
-		this.events.push(new TimelineEvent('Title 1', new Date(), 'Text 1', false, 'icon-download'));
-		this.events.push(new TimelineEvent('Title 2', new Date(), 'Text 2', true, 'icon-comment'));
-		this.events.push(new TimelineEvent('Title 3', new Date(), 'Text 3', false, 'icon-plus'));
-		this.events.push(new TimelineEvent('Title 4', new Date(), 'text 4', true, 'icon-home'));
+		const timeLineEvent1 = new TimelineEvent('Title 1', new Date(), 'Text 1');
+		timeLineEvent1.inverted = false;
+		timeLineEvent1.icon = 'icon-download';
+		this.events.push(timeLineEvent1);
+		const timeLineEvent2 = new TimelineEvent('Title 2', new Date(), 'Text 2');
+		timeLineEvent2.inverted = true;
+		timeLineEvent2.icon = 'icon-comment';
+		this.events.push(timeLineEvent2);
+		const timeLineEvent3 = new TimelineEvent('Title 3', new Date(), 'Text 3');
+		timeLineEvent3.inverted = false;
+		timeLineEvent3.icon = 'icon-plus';
+		this.events.push(timeLineEvent3);
+		const timeLineEvent4 = new TimelineEvent('Title 4', new Date(), 'text 4');
+		timeLineEvent4.inverted = true;
+		timeLineEvent4.icon = 'icon-home';
+		this.events.push(timeLineEvent4);
 	}
 }
 
@@ -64,18 +76,24 @@ describe('Systelab Timeline', () => {
 	});
 
 	it('should have the right title', () => {
-		expect(getTitle(fixture, 0)).toEqual(fixture.componentInstance.events[0].title);
-		expect(getTitle(fixture, 2)).toEqual(fixture.componentInstance.events[2].title);
+		expect(getTitle(fixture, 0))
+			.toEqual(fixture.componentInstance.events[0].title);
+		expect(getTitle(fixture, 2))
+			.toEqual(fixture.componentInstance.events[2].title);
 	});
 
 	it('should have the right text', () => {
-		expect(getText(fixture, 0)).toEqual(fixture.componentInstance.events[0].text);
-		expect(getText(fixture, 2)).toEqual(fixture.componentInstance.events[2].text);
+		expect(getText(fixture, 0))
+			.toEqual(fixture.componentInstance.events[0].text);
+		expect(getText(fixture, 2))
+			.toEqual(fixture.componentInstance.events[2].text);
 	});
 
 	it('should have the right icon', () => {
-		expect(getImage(fixture, 0)).toContain(fixture.componentInstance.events[0].icon);
-		expect(getImage(fixture, 2)).toContain(fixture.componentInstance.events[2].icon);
+		expect(getImage(fixture, 0))
+			.toContain(fixture.componentInstance.events[0].icon);
+		expect(getImage(fixture, 2))
+			.toContain(fixture.componentInstance.events[2].icon);
 	});
 });
 
