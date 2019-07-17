7a07f36a123f1c506ccced3a14cb2bef54ae7c54
Alfons Serra <alfonsoserra@gmail.com>
2019-07-17 11:36:48 +0200

    set the possibility to add a submenu for grid (#254)

--- src/app/showcase/components/grid/showcase-grid.component.ts
+++ src/app/showcase/components/grid/showcase-grid.component.ts
@@ -24,12 +24,22 @@ export class ShowcaseGridComponent implements OnInit {
 	}
 
 	public getMenu(): Array<GridContextMenuOption<ShowcaseData>> {
+
+
+		const contextMenuSubOptions: Array<GridContextMenuOption<ShowcaseData>> = [
+			new GridContextMenuOption('option1', 'Option 1', (a) => this.doMenuAction(a)),
+			new GridContextMenuOption('option2', 'Option 2', (a) => this.doMenuAction(a)),
+			new GridContextMenuOption('option5', 'Option 5', (a) => this.doMenuAction(a)),
+		];
+
 		return [
 			new GridContextMenuOption('action1', 'Action 1', (a) => this.doMenuAction(a)),
 			new GridContextMenuOption('action2', 'Action 2', (a) => this.doMenuAction(a)),
 			new GridContextMenuOption('', '', (a) => this.doMenuAction(a), null, true),
-			new GridContextMenuOption('action3', 'Action 3', (a) => this.doMenuAction(a)),
-			new GridContextMenuOption('action4', 'Action 4', (a) => this.doMenuAction(a), () => false)
+			new GridContextMenuOption('action3', 'Action 3', null, () => true, false, undefined, null, null, contextMenuSubOptions),
+			new GridContextMenuOption('option4', 'Action 4', null, () => true, false, undefined, null, null, contextMenuSubOptions),
+			new GridContextMenuOption('action5', 'Action 5', (a) => this.doMenuAction(a)),
+			new GridContextMenuOption('action6', 'Action 6', (a) => this.doMenuAction(a), () => false)
 		];
 	}
 
