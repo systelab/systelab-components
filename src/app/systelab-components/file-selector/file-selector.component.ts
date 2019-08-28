f206f2e2277b05ee611f4c2e33369a2d15f53567
rcarmona <rcarmona@werfen.com>
2019-08-28 09:16:31 +0200

    Systelab-file-selector: - Added disabled behaviour. - Added File and multiple FileList selection. - Added different ways to shown the selector button under different scenarios. - Added Unit Testing. - README.md updated.

--- src/app/systelab-components/file-selector/file-selector.component.ts
+++ src/app/systelab-components/file-selector/file-selector.component.ts
@@ -4,13 +4,23 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 	selector: 'systelab-file-selector',
 	templateUrl: 'file-selector.component.html'
 })
-export class FileSelectorComponent  {
+
+export class FileSelectorComponent {
 
 	private _fileName = '';
+	private _file: File;
+	private _fileList: FileList;
 
-	@Output() public fileNameChange = new EventEmitter();
+	@Output() public fileNameChange = new EventEmitter<string>();
+	@Output() public fileChange = new EventEmitter<File>();
+	@Output() public fileListChange = new EventEmitter<FileList>();
 
-	@Input() public get fileName() {
+	@Input() showButtonOnDisable = false;
+	@Input() multipleSelection = false;
+	@Input() disabled = false;
+
+	@Input()
+	public get fileName() {
 		return this._fileName;
 	}
 
@@ -19,9 +29,38 @@ export class FileSelectorComponent  {
 		this.fileNameChange.emit(this._fileName);
 	}
 
+	@Input()
+	public get file() {
+		return this._file;
+	}
+
+	public set file(file: File) {
+		this._file = file;
+		this.fileChange.emit(this._file);
+	}
+
+	@Input()
+	public get fileList() {
+		return this._fileList;
+	}
+
+	public set fileList(fileList: FileList) {
+		this._fileList = fileList;
+		this.fileListChange.emit(this._fileList);
+	}
+
 	public selectFile(files: FileList) {
 		if (files.length) {
-			this.fileName = files[0].name;
+			if (this.multipleSelection) {
+				this.fileList = files;
+				this.fileName = '';
+				for (let index = 0; index < files.length; index++) {
+					this.fileName += (this.fileName.length > 0 ? ', ' : '') + files[index].name;
+				}
+			} else {
+				this.fileName = files[0].name;
+				this.file = files[0];
+			}
 		}
 	}
 }
