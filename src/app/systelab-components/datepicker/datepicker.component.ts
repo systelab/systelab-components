db12c28b108e076122fdcfa3a933ef854940f639
alfonsserra <alfonsoserra@gmail.com>
2019-11-08 14:53:43 +0100

    Move the transformations to a service

--- src/app/systelab-components/datepicker/datepicker.component.ts
+++ src/app/systelab-components/datepicker/datepicker.component.ts
@@ -1,12 +1,13 @@
 import { AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { Calendar } from 'primeng/components/calendar/calendar';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
-import { addDays, addMonths, addWeeks, addYears } from 'date-fns';
-import { first } from 'rxjs/operators';
+import { addDays } from 'date-fns';
+import { DataTransformerService } from './date-transformer.service';
 
 @Component({
 	selector:    'systelab-datepicker',
-	templateUrl: 'datepicker.component.html'
+	templateUrl: 'datepicker.component.html',
+	providers:   [DataTransformerService]
 })
 export class Datepicker implements OnInit, AfterViewInit, DoCheck, OnDestroy {
 
@@ -65,7 +66,7 @@ export class Datepicker implements OnInit, AfterViewInit, DoCheck, OnDestroy {
 
 	private headerElement: any = document.getElementById(this.datepickerId);
 
-	constructor(protected myRenderer: Renderer2, protected i18nService: I18nService) {
+	constructor(protected myRenderer: Renderer2, protected i18nService: I18nService, private dataTransformerService: DataTransformerService) {
 		this.addListeners();
 		// TODO: To get the language and modify the values.
 	}
@@ -88,7 +89,8 @@ export class Datepicker implements OnInit, AfterViewInit, DoCheck, OnDestroy {
 			newElement.className = 'icon-calendar';
 			if (this.currentCalendar) {
 				if (this.autofocus) {
-					this.currentCalendar.el.nativeElement.querySelector('input').focus();
+					this.currentCalendar.el.nativeElement.querySelector('input')
+						.focus();
 				}
 				this.currentCalendar.el.nativeElement.childNodes[0].className = 'ui-calendar slab-form-icon w-100';
 				this.currentCalendar.el.nativeElement.childNodes[0].appendChild(newElement);
@@ -149,156 +151,21 @@ export class Datepicker implements OnInit, AfterViewInit, DoCheck, OnDestroy {
 
 	public changeDate(): void {
 		if (this.currentCalendar && this.currentCalendar.inputfieldViewChild.nativeElement.value !== undefined) {
-			const today = new Date();
 			const dateStr = this.currentCalendar.inputfieldViewChild.nativeElement.value.trim().toLowerCase();
 			if (this.inputChanged && dateStr.length >= 2) {
-				if (dateStr.toUpperCase().endsWith('D')) {
-					this.currentDate = addDays(today, this.getAmount(dateStr, 'D'));
-				} else if (dateStr.toUpperCase().endsWith('W') || dateStr.toUpperCase().endsWith('S')) {
-					this.currentDate = addWeeks(today, this.getAmount(dateStr, 'W', 'S'));
-				} else if (dateStr.toUpperCase().endsWith('M')) {
-					this.currentDate = addMonths(today, this.getAmount(dateStr, 'M'));
-				} else if (dateStr.toUpperCase().endsWith('Y') || dateStr.toUpperCase().endsWith('A')) {
-					this.currentDate = addYears(today, this.getAmount(dateStr, 'Y', 'A'));
+				const transformedDate = this.dataTransformerService.processShortcuts(dateStr);
+				if (transformedDate) {
+					this.currentDate = transformedDate;
 				} else {
-					const transformedDate = this.transformDateWithoutSeparator(dateStr);
-					if (transformedDate) {
-						this.currentDate = transformedDate;
+					const inferedDate = this.dataTransformerService.infereDate(dateStr, this.i18nService.getDateFormat());
+					if (inferedDate) {
+						this.currentDate = inferedDate;
 					}
 				}
 				this.currentDateChange.emit(this.currentDate);
 				this.inputChanged = false;
 			}
 		}
-	}
-
-	private getAmount(dateStr: string, ...symbols: string[]): number {
-		for (const symbol of symbols) {
-			if (dateStr.toUpperCase().endsWith(symbol.toUpperCase())) {
-				const amount = Number(dateStr.toUpperCase().replace(symbol.toUpperCase(), ''));
-				if (!isNaN(amount)) {
-					return amount;
-				}
-			}
-		}
-		return 0;
-	}
-
-	public transformDateWithoutSeparator(date: string): Date {
-		let dateTmp = date.trim();
-
-		const dateFormat: string = this.i18nService.getDateFormat();
-		const dayBefore = dateFormat.toUpperCase().lastIndexOf( 'D') < dateFormat.toUpperCase().lastIndexOf( 'M');
-
-		let dateSeparator: string;
-
-		let firstSeparatorPosition: number;
-		let secondSeparatorPosition: number;
-
-		if (this.hasSeparator(dateTmp)) {
-			// Get separator positions before removing them
-			dateSeparator = this.getDateSeparator(dateFormat);
-			firstSeparatorPosition = dateTmp.indexOf(dateSeparator);
-			secondSeparatorPosition = dateTmp.lastIndexOf(dateSeparator);
-
-			dateTmp = this.removeSeparator(dateTmp, dateSeparator);
-			// After removing the desired separator, no other separator is expected
-			if (this.hasSeparator(dateTmp)) {
-				return undefined;
-			}
-		}
-
-
-		return this.getFormattedDate(dateTmp, dayBefore, firstSeparatorPosition, secondSeparatorPosition);
-	}
-
-	private getFormattedDate(dateTmp: string, dayBefore: boolean,  firstSeparatorPosition: number, secondSeparatorPosition: number): Date {
-		if (dateTmp.length === 4) {
-			// Manage dates with format d/m/yy or dmyy or m/d/yy or mdyy
-			return this.getFormattedDateFourDigits (dateTmp, dayBefore);
-		} else if (dateTmp.length === 6 || dateTmp.length === 8) {
-			// Manage dates with format dd/mm/yy or ddmmyy or mm/dd/yy or mmddyy or dd/mm/yyyy or mm/dd/yyyy
-			return this.getFormattedDateSixOrEigthDigits(dateTmp, dayBefore);
-		} else if ((dateTmp.length === 5 || dateTmp.length === 7) && firstSeparatorPosition > 0 && secondSeparatorPosition !== firstSeparatorPosition) {
-			// Manage dates with format dd/m/yy or mm/d/yy or d/mm/yy or m/dd/yy or all before but with year with 4 digits
-			return this.getFormattedDateFiveOrSevenDigits(dateTmp, dayBefore, firstSeparatorPosition);
-		}
-
-		return undefined;
-	}
-
-	private getFormattedDateFourDigits (dateTmp, dayBefore): Date {
-		let dayInDate: number;
-		let monthInDate: number;
-		let yearInDate: number;
-		if (dayBefore) {
-			dayInDate = +dateTmp.substring(0, 1);
-			monthInDate = +dateTmp.substring(1, 2) - 1;
-		} else {
-			monthInDate = +dateTmp.substring(0, 1) - 1;
-			dayInDate = +dateTmp.substring(1, 2);
-		}
-		yearInDate = +dateTmp.substring(2);
-		if (yearInDate < 100) {
-			yearInDate = 2000 + yearInDate;
-		}
-		return new Date(yearInDate, monthInDate, dayInDate);
-	}
-
-	private getFormattedDateSixOrEigthDigits (dateTmp, dayBefore): Date {
-		let dayInDate: number;
-		let monthInDate: number;
-		let yearInDate: number;
-		if (dayBefore) {
-			dayInDate = +dateTmp.substring(0, 2);
-			monthInDate = +dateTmp.substring(2, 4) - 1;
-		} else {
-			monthInDate = +dateTmp.substring(0, 2) - 1;
-			dayInDate = +dateTmp.substring(2, 4);
-		}
-		yearInDate = +dateTmp.substring(4);
-		if (yearInDate < 100) {
-			yearInDate = 2000 + yearInDate;
-		}
-		return new Date(yearInDate, monthInDate, dayInDate);
-	}
-
-	private getFormattedDateFiveOrSevenDigits (dateTmp, dayBefore, firstSeparatorPosition): Date {
-		let dayInDate: number;
-		let monthInDate: number;
-		let yearInDate: number;
-		if (dayBefore) {
-			dayInDate = +dateTmp.substring(0, firstSeparatorPosition);
-			monthInDate = +dateTmp.substring(firstSeparatorPosition, 3) - 1;
-		} else {
-			monthInDate = +dateTmp.substring(0, firstSeparatorPosition) - 1;
-			dayInDate = +dateTmp.substring(firstSeparatorPosition, 3);
-		}
-		yearInDate = +dateTmp.substring(3);
-		if (yearInDate < 100) {
-			yearInDate = 2000 + yearInDate;
-		}
-		return new Date(yearInDate, monthInDate, dayInDate);
-	}
-
-	private getDateSeparator(dateFormat: string) {
-		let  dateSeparator: string;
-		if (dateFormat.lastIndexOf('/') > 0) {
-			dateSeparator = '/';
-		} else if (dateFormat.lastIndexOf('-') > 0) {
-			dateSeparator = '-';
-		} else if (dateFormat.lastIndexOf('.') > 0) {
-			dateSeparator = '.';
-		}
-		return dateSeparator;
-	}
-
-	private hasSeparator(dateTmp: string): boolean {
-		return dateTmp.includes('/') || dateTmp.includes('-') || dateTmp.includes('.');
-	}
-
-	private removeSeparator(dateTmp: string, dateSeparator: string): string {
-		return dateTmp.split(dateSeparator).join('');
 	}
 
 	public onInput(event: KeyboardEvent) {
