5919d692ee431ec716b21065915a788f4364f283
vinyulis <vinyulis@gmail.com>
2019-12-18 16:54:59 +0100

    - Reduce number of lines with a method based on day, month, year length.

--- src/app/systelab-components/datepicker/date-transformer.service.ts
+++ src/app/systelab-components/datepicker/date-transformer.service.ts
@@ -30,135 +30,92 @@ export class DataTransformerService {
 	public infereDate(date: string, dateFormat: string): Date {
 		let dateTmp = date.trim();
 
-		const dayPosition = dateFormat.toUpperCase()
-			.lastIndexOf('D');
-		const monthPosition = dateFormat.toUpperCase()
-			.lastIndexOf('M');
-		const yearPosition = dateFormat.toUpperCase()
-			.lastIndexOf('Y');
+		const dayPosition = dateFormat.lastIndexOf('d');
+		const monthPosition = dateFormat.lastIndexOf('m');
+		const yearPosition = dateFormat.lastIndexOf('y');
+
 		const dayBefore = dayPosition < monthPosition;
 		const yearBefore = yearPosition < dayPosition;
-
-		let dateSeparator: string;
 
 		let firstSeparatorPosition: number;
 		let secondSeparatorPosition: number;
 
-		if (this.hasSeparator(dateTmp)) {
+		const separator = this.getDateSeparator(dateFormat);
+		if (separator) {
 			// Get separator positions before removing them
-			dateSeparator = this.getDateSeparator(dateFormat);
-			firstSeparatorPosition = dateTmp.indexOf(dateSeparator);
-			secondSeparatorPosition = dateTmp.lastIndexOf(dateSeparator);
+			firstSeparatorPosition = dateTmp.indexOf(separator);
+			secondSeparatorPosition = dateTmp.lastIndexOf(separator);
 
-			dateTmp = this.removeSeparator(dateTmp, dateSeparator);
+			dateTmp = this.removeSeparator(dateTmp, separator);
 			// After removing the desired separator, no other separator is expected
-			if (this.hasSeparator(dateTmp)) {
+			if (this.getDateSeparator(dateTmp)) {
 				return undefined;
 			}
 		}
 
-		return this.getFormattedDate(dateTmp, dayBefore, yearBefore, firstSeparatorPosition, secondSeparatorPosition);
-	}
+		const dayLength = this.getDayLength(dateTmp, dayBefore, yearBefore, firstSeparatorPosition, secondSeparatorPosition);
+		const monthLength = [4, 6, 8].some(length => dateTmp.length === length) ? dayLength : 2 / dayLength;
+		const yearLength = [7, 8].some(length => dateTmp.length === length) ? 4 : 2;
 
-	private getFormattedDate(dateTmp: string, dayBefore: boolean, yearBefore: boolean, firstSeparatorPosition: number, secondSeparatorPosition: number): Date {
-		if (dateTmp.length === 4) {
-			// Manage dates with format d/m/yy or dmyy or m/d/yy or mdyy
-			return this.getFormattedDateFourDigits(dateTmp, dayBefore, yearBefore);
-		} else if (dateTmp.length === 6 || dateTmp.length === 8) {
-			// Manage dates with format dd/mm/yy or ddmmyy or mm/dd/yy or mmddyy or dd/mm/yyyy or mm/dd/yyyy
-			return this.getFormattedDateSixOrEigthDigits(dateTmp, dayBefore, yearBefore);
-		} else if ((dateTmp.length === 5 || dateTmp.length === 7) && firstSeparatorPosition > 0 && secondSeparatorPosition !== firstSeparatorPosition) {
-			// Manage dates with format dd/m/yy or mm/d/yy or d/mm/yy or m/dd/yy or all before but with year with 4 digits
-			return this.getFormattedDateFiveOrSevenDigits(dateTmp, dayBefore, yearBefore, firstSeparatorPosition, secondSeparatorPosition);
+		if (dayLength && monthLength && yearLength) {
+			return this.getFormattedDate(dateTmp, dayBefore, yearBefore, dayLength, monthLength, yearLength);
 		}
 
 		return undefined;
 	}
 
-	private getFormattedDateFourDigits(dateTmp: string, dayBefore: boolean, yearBefore: boolean): Date {
-		let dayInDate: number;
-		let monthInDate: number;
-		let yearInDate: number;
-		if (yearBefore) {
-			yearInDate = +dateTmp.substring(0, 2);
-			if (dayBefore) {
-				dayInDate = +dateTmp.substring(2, 3);
-				monthInDate = +dateTmp.substring(3) - 1;
-			} else {
-				monthInDate = +dateTmp.substring(2, 3) - 1;
-				dayInDate = +dateTmp.substring(3);
-			}
-		} else {
-			if (dayBefore) {
-				dayInDate = +dateTmp.substring(0, 1);
-				monthInDate = +dateTmp.substring(1, 2) - 1;
-			} else {
-				monthInDate = +dateTmp.substring(0, 1) - 1;
-				dayInDate = +dateTmp.substring(1, 2);
-			}
-			yearInDate = +dateTmp.substring(2);
-		}
+	private getDayLength(date: string, dayBefore: boolean, yearBefore: boolean, firstSeparatorPosition: number, secondSeparatorPosition: number): number {
+		switch (date.length) {
+			case 4:
+				return 1;
+			case 6:
+			case 8:
+				return 2;
+			case 5:
+			case 7:
+				if (firstSeparatorPosition > 0 && secondSeparatorPosition !== firstSeparatorPosition) {
+					let dayLength: number;
+					if (secondSeparatorPosition - firstSeparatorPosition === 2) {
+						dayLength = 1;
+					} else {
+						dayLength = 2;
+					}
+					if (!dayBefore) {
+						dayLength = 2 / dayLength;
+					}
+					if (!yearBefore) {
+						dayLength = 2 / dayLength;
 
-		if (yearInDate < 100) {
-			yearInDate = 2000 + yearInDate;
+					}
+					return dayLength;
+				}
 		}
-		return new Date(yearInDate, monthInDate, dayInDate);
 	}
 
-	private getFormattedDateSixOrEigthDigits(dateTmp: string, dayBefore: boolean, yearBefore: boolean): Date {
+	private getFormattedDate(dateTmp: string, dayBefore: boolean, yearBefore: boolean, dayLength: number, monthLength: number, yearLength): Date {
 		let dayInDate: number;
 		let monthInDate: number;
 		let yearInDate: number;
-		const dateLength = dateTmp.length;
 		if (yearBefore) {
-			yearInDate = +dateTmp.substring(0, dateLength - 4);
+			yearInDate = +dateTmp.substring(0, yearLength);
 			if (dayBefore) {
-				dayInDate = +dateTmp.substring(dateLength - 4, dateLength - 2);
-				monthInDate = +dateTmp.substring(dateLength - 2) - 1;
+				dayInDate = +dateTmp.substring(yearLength, yearLength + dayLength);
+				monthInDate = +dateTmp.substring(yearLength + dayLength) - 1;
 			} else {
-				monthInDate = +dateTmp.substring(dateLength - 4, dateLength - 2) - 1;
-				dayInDate = +dateTmp.substring(dateLength - 2);
+				monthInDate = +dateTmp.substring(yearLength, yearLength + monthLength) - 1;
+				dayInDate = +dateTmp.substring(yearLength + monthLength);
 			}
 		} else {
 			if (dayBefore) {
-				dayInDate = +dateTmp.substring(0, 2);
-				monthInDate = +dateTmp.substring(2, 4) - 1;
+				dayInDate = +dateTmp.substring(0, dayLength);
+				monthInDate = +dateTmp.substring(dayLength, dayLength + monthLength) - 1;
 			} else {
-				monthInDate = +dateTmp.substring(0, 2) - 1;
-				dayInDate = +dateTmp.substring(2, 4);
+				monthInDate = +dateTmp.substring(0, monthLength) - 1;
+				dayInDate = +dateTmp.substring(monthLength, monthLength + dayLength);
 			}
-			yearInDate = +dateTmp.substring(4);
+			yearInDate = +dateTmp.substring(monthLength + dayLength);
 		}
-		if (yearInDate < 100) {
-			yearInDate = 2000 + yearInDate;
-		}
-		return new Date(yearInDate, monthInDate, dayInDate);
-	}
 
-	private getFormattedDateFiveOrSevenDigits(dateTmp: string, dayBefore: boolean, yearBefore: boolean, firstSeparatorPosition: number, secondSeparatorPosition: number): Date {
-		let dayInDate: number;
-		let monthInDate: number;
-		let yearInDate: number;
-		const dateLength = dateTmp.length;
-		if (yearBefore) {
-			yearInDate = +dateTmp.substring(0, dateLength - 3);
-			if (dayBefore) {
-				dayInDate = +dateTmp.substring(dateLength - 3, secondSeparatorPosition - 1);
-				monthInDate = +dateTmp.substring(secondSeparatorPosition) - 1;
-			} else {
-				monthInDate = +dateTmp.substring(dateLength - 3, secondSeparatorPosition - 1) - 1;
-				dayInDate = +dateTmp.substring(secondSeparatorPosition - 1);
-			}
-		} else {
-			if (dayBefore) {
-				dayInDate = +dateTmp.substring(0, firstSeparatorPosition);
-				monthInDate = +dateTmp.substring(firstSeparatorPosition, secondSeparatorPosition - 1) - 1;
-			} else {
-				monthInDate = +dateTmp.substring(0, firstSeparatorPosition) - 1;
-				dayInDate = +dateTmp.substring(firstSeparatorPosition, secondSeparatorPosition - 1);
-			}
-			yearInDate = +dateTmp.substring(secondSeparatorPosition - 1);
-		}
 		if (yearInDate < 100) {
 			yearInDate = 2000 + yearInDate;
 		}
@@ -167,18 +124,14 @@ export class DataTransformerService {
 
 	private getDateSeparator(dateFormat: string) {
 		let dateSeparator: string;
-		if (dateFormat.lastIndexOf('/') > 0) {
+		if (dateFormat.includes('/')) {
 			dateSeparator = '/';
-		} else if (dateFormat.lastIndexOf('-') > 0) {
+		} else if (dateFormat.includes('-')) {
 			dateSeparator = '-';
-		} else if (dateFormat.lastIndexOf('.') > 0) {
+		} else if (dateFormat.includes('.')) {
 			dateSeparator = '.';
 		}
 		return dateSeparator;
-	}
-
-	private hasSeparator(dateTmp: string): boolean {
-		return dateTmp.includes('/') || dateTmp.includes('-') || dateTmp.includes('.');
 	}
 
 	private removeSeparator(dateTmp: string, dateSeparator: string): string {
