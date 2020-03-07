82fe4b9b2b213dd17243b7495a729dfa00c7149e
alfonsserra <alfonsoserra@gmail.com>
2020-03-07 07:12:52 +0100

    Inject the service

--- src/app/showcase/components/grid/showcase-inner-bars-grid.component.ts
+++ src/app/showcase/components/grid/showcase-inner-bars-grid.component.ts
@@ -4,6 +4,7 @@ import { PreferencesService } from 'systelab-preferences/lib/preferences.service
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 import { DialogService } from '../../../systelab-components/modal';
 import { ShowcaseInnerGridComponent } from './showcase-inner-grid.component';
+import { GridColumnOptionsService } from '../../../systelab-components/grid/options/grid-column-options.service';
 
 @Component({
 	selector:    'showcase-inner-bars-grid',
@@ -12,8 +13,9 @@ import { ShowcaseInnerGridComponent } from './showcase-inner-grid.component';
 })
 export class ShowcaseBarsGridComponent extends ShowcaseInnerGridComponent implements OnInit {
 
-	constructor(protected preferencesService: PreferencesService, protected i18nService: I18nService, protected dialogService: DialogService) {
-		super(preferencesService, i18nService, dialogService);
+	constructor(protected preferencesService: PreferencesService, protected i18nService: I18nService,
+	            protected dialogService: DialogService, protected gridColumnOptionsService: GridColumnOptionsService) {
+		super(preferencesService, i18nService, dialogService, gridColumnOptionsService);
 	}
 
 	protected getColumnDefs(): Array<any> {
