085560f5533b14d6f62180a12f6e25a3e2578a21
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2020-10-14 11:30:56 +0200

    Revert Fix dialog: Dialogs with long titles bug in Firefox #398 It causes collaterals

--- projects/systelab-components/src/lib/modal/dialog/dialog.service.ts
+++ projects/systelab-components/src/lib/modal/dialog/dialog.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Injector, Renderer2, RendererFactory2, Type } from '@angular/core';
+import { Injectable, Injector, Type } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
@@ -9,18 +9,15 @@ import { SystelabModalContext } from './modal-context';
 export class DialogService {
 
 	public static readonly breakpointMedium = 768;
-	private renderer: Renderer2;
 
-	constructor(private readonly overlay: Overlay, private injector: Injector, private readonly rendererFactory: RendererFactory2) {
-		this.renderer = this.rendererFactory.createRenderer(undefined, undefined);
+	constructor(private readonly overlay: Overlay, private injector: Injector) {
 	}
 
 	public showDialog(component: Type<any>, parameters: SystelabModalContext): Observable<any> {
 		const overlayRef = this.overlay.create(this.getConfig(parameters));
 		const dialogRef = new DialogRef(overlayRef, parameters);
 		const userProfilePortal = new ComponentPortal(component, null, this.createInjector(dialogRef));
-		const componentRef = overlayRef.attach(userProfilePortal);
-		this.renderer.addClass(componentRef.location.nativeElement, 'slab-overflow-container');
+		overlayRef.attach(userProfilePortal);
 		return dialogRef.getResult();
 	}
 
