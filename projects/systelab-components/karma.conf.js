e6f4c2fb79f400d15546302380d8f60d64f7aff9
opuig <Paula_2017>
2021-01-22 10:13:21 +0100

    Use 'karma-coverage' because 'karma-coverage-istanbul-reporter' usage has been deprecated since version 11

--- projects/systelab-components/karma.conf.js
+++ projects/systelab-components/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function(config) {
 	config.set({
@@ -9,25 +9,32 @@ module.exports = function(config) {
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
 			require('karma-jasmine-html-reporter'),
-			require('karma-coverage-istanbul-reporter'),
+			require('karma-coverage'),
 			require('@angular-devkit/build-angular/plugins/karma')
 		],
 		client:                   {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
-		coverageIstanbulReporter: {
-			dir: require('path').join(__dirname, '../../coverage/systelab-components'), reports:               ['lcov'],
+		coverageReporter: {
+			dir: require('path').join(__dirname, '../../coverage/systelab-components'),
+			subdir: '.',
+			reporters: [
+				{ type: 'lcov' },
+				{ type: 'html' },
+				{ type: 'text-summary' }
+			],
 			fixWebpackSourcePaths: true
 		},
 		angularCli:               {
 			environment: 'dev'
 		},
-		reporters:                ['progress', 'kjhtml', 'coverage-istanbul'],
+		reporters:                ['progress', 'kjhtml'],
 		port:                     9876,
 		colors:                   true,
 		logLevel:                 config.LOG_INFO,
 		autoWatch:                true,
 		browsers:                 ['Chrome'],
-		singleRun:                false
+		singleRun:                false,
+		restartOnFileChange: true
 	});
 };
