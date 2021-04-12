94d9be2d46ac450423a4cf35a33ed9309c1d0b66
alfonsserra <alfonsoserra@gmail.com>
2021-04-12 13:33:54 +0200

    Fixes #451. Adds support for decimal precision for spinners.

--- projects/showcase/src/app/components/spinner/showcase-spinner.component.ts
+++ projects/showcase/src/app/components/spinner/showcase-spinner.component.ts
@@ -9,10 +9,12 @@ export class ShowcaseSpinnerComponent {
 	public touchSpinValues1: TouchSpinValues;
 	public touchSpinValues2: TouchSpinValues;
 	public touchSpinValues3: TouchSpinValues;
+	public touchSpinValues4: TouchSpinValues;
 
 	constructor() {
 		this.touchSpinValues1 = new TouchSpinValues(1, 1, 10);
 		this.touchSpinValues2 = new TouchSpinValues(5, 1, 20, 2);
 		this.touchSpinValues3 = new TouchSpinValues(0, -10, 10, 1);
+		this.touchSpinValues4 = new TouchSpinValues(0, -10, 10, 0.001, true, 3);
 	}
 }
