826dd5f89df62bf85b4b361b82405095393f9a0f
odedios <odedios@werfen.com>
2021-10-20 17:01:52 +0200

    Fix i18n in calendar component

--- projects/systelab-components/src/lib/datepicker/datepicker-time.component.ts
+++ projects/systelab-components/src/lib/datepicker/datepicker-time.component.ts
@@ -3,6 +3,7 @@ import { Datepicker } from './datepicker.component';
 import { TouchSpinValues } from '../spinner/touch.spin-values';
 import { I18nService } from 'systelab-translate';
 import { DataTransformerService } from './date-transformer.service';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
 	selector:    'systelab-date-time',
@@ -20,8 +21,8 @@ export class DatepickerTimeComponent extends Datepicker {
 	public touchSpinHourValues: TouchSpinValues;
 	public touchSpinMinutesValues: TouchSpinValues;
 
-	constructor(myRenderer: Renderer2, i18nService: I18nService, dataTransformerService: DataTransformerService) {
-		super(myRenderer, i18nService, dataTransformerService);
+	constructor(myRenderer: Renderer2, i18nService: I18nService, dataTransformerService: DataTransformerService, config: PrimeNGConfig) {
+		super(myRenderer, i18nService, dataTransformerService, config);
 
 		this.touchSpinHourValues = new TouchSpinValues(0, 0, 23, 1);
 		this.touchSpinMinutesValues = new TouchSpinValues(0, 0, 59, 1);
