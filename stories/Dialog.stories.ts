ae522fa69d773eddb3759d33524f0348419b4f54
alfonsserra <alfonsoserra@gmail.com>
2021-11-12 09:45:25 +0100

    Slider and Dialog example

---
+++ stories/Dialog.stories.ts
@@ -0,0 +1,106 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import DialogDocumentation from './docs/dialog.mdx';
+import { moduleMetadata } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { DialogService } from '../projects/systelab-components/src/lib/modal/dialog/dialog.service';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { DialogRef } from '../projects/systelab-components/src/lib/modal/dialog/dialog-ref';
+import { ModalComponent } from '../projects/systelab-components/src/lib/modal/dialog/modal-context';
+import { SystelabModalContext } from '../projects/systelab-components/src/lib/modal/dialog/modal-context';
+import { DialogHeaderComponent } from '../projects/systelab-components/src/lib/modal/header/dialog-header.component';
+import { DialogBottomComponent } from '../projects/systelab-components/src/lib/modal/bottom/dialog-bottom.component';
+
+
+
+class ShowcaseStandardDialogParameters extends SystelabModalContext {
+  public index: number;
+  public width = 500;
+  public height = 400;
+}
+
+@Component({
+  template: `<div class="slab-flex-1 d-flex flex-column" cdkTrapFocus>
+    <systelab-dialog-header (close)="close();">Title</systelab-dialog-header>
+    <div class="slab-flex-1"></div>
+    <systelab-dialog-bottom>
+      <button type="button" class="btn btn-primary ml-auto" (click)="close()"> Submit</button>
+    </systelab-dialog-bottom>
+  </div>
+            `,
+})
+class ShowcaseStandardDialog implements ModalComponent<ShowcaseStandardDialogParameters> {
+
+  protected parameters: ShowcaseStandardDialogParameters;
+
+  public static getParameters(): ShowcaseStandardDialogParameters {
+    return new ShowcaseStandardDialogParameters();
+  }
+
+  constructor(public dialog: DialogRef<ShowcaseStandardDialogParameters>) {
+    this.parameters = dialog.context;
+  }
+
+  public close(): void {
+    this.dialog.close('This is a test');
+  }
+}
+
+
+
+
+@Component({
+  selector: "app-modal-story",
+  template: `
+		<button class="btn btn-primary" (click)="openModal()">Open Dialog</button>
+	`
+})
+class ModalStory {
+
+  @Input() modalText = "Hello, World";
+
+  @Input() size = "default";
+
+  @Input() showCloseButton = true;
+
+  constructor(protected dialogService: DialogService) { }
+
+  openModal() {
+    this.dialogService.showDialog(ShowcaseStandardDialog, ShowcaseStandardDialog.getParameters());
+
+    console.log('Do something');
+  }
+}
+
+
+export default {
+  title: 'Navigation/Dialogs',
+  component: ModalStory,
+  decorators: [
+    moduleMetadata({
+      declarations: [ModalStory, ShowcaseStandardDialog, DialogHeaderComponent, DialogBottomComponent],
+      imports: [CommonModule, OverlayModule],
+    }),
+  ],
+  parameters: {
+    docs: {
+      page: DialogDocumentation,
+    }
+  },
+} as Meta;
+
+const Template: Story<ModalStory> = (args: ModalStory) => ({
+  props: args,
+  template: `
+    <div class="container">
+    <app-modal-story></app-modal-story>
+    </div>
+    `,
+
+});
+
+
+export const Default = Template.bind({});
+Default.args = {
+};
+
---
+++ stories/Slider.stories.ts
@@ -0,0 +1,37 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import SliderDocumentation from './docs/slider.mdx';
+import { SliderComponent } from '../projects/systelab-components/src/lib/slider/slider.component';
+import { moduleMetadata } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+
+
+export default {
+	title: 'Navigation/Slider',
+	component: SliderComponent,
+	decorators: [
+		moduleMetadata({
+			declarations: [SliderComponent],
+			imports: [CommonModule],
+		}),
+	],
+	parameters: {
+		docs: {
+			page: SliderDocumentation,
+		}
+	},
+} as Meta;
+
+const Template: Story<SliderComponent> = (args: SliderComponent) => ({
+	props: args,
+	template: `
+    <div class="container">
+    <systelab-slider [continuous]="true" [max]="100" [min]="0" [step]="1"></systelab-slider>
+    </div>
+    `,
+
+});
+
+
+export const Default = Template.bind({});
+Default.args = {
+};
