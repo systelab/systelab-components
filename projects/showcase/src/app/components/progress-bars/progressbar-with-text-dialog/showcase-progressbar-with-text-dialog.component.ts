a12134fdb7d224a1f0b9a5b1a21229e614368e02
Carlos Rodriguez Aguado <crodriguez2@werfen.com>
2022-03-01 12:44:14 +0100

    Create advanced progress bar with text #574

---
+++ projects/showcase/src/app/components/progress-bars/progressbar-with-text-dialog/showcase-progressbar-with-text-dialog.component.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild } from '@angular/core';
+import { DialogHeaderComponent, DialogRef, ModalComponent, SystelabModalContext } from 'systelab-components';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
+export class ShowcaseProgressBarWithTextDialogParameters extends SystelabModalContext {
+	public index: number;
+}
+
+@Component({
+	templateUrl: 'showcase-progressbar-with-text-dialog.component.html',
+})
+export class ShowcaseProgressBarWithTextDialog implements ModalComponent<ShowcaseProgressBarWithTextDialogParameters> {
+
+	@ViewChild('header', {static: false}) header: DialogHeaderComponent;
+
+	protected parameters: ShowcaseProgressBarWithTextDialogParameters;
+	private progress = 0;
+
+	public static getParameters(): ShowcaseProgressBarWithTextDialogParameters {
+		return new ShowcaseProgressBarWithTextDialogParameters();
+	}
+
+	constructor(public dialog: DialogRef<ShowcaseProgressBarWithTextDialogParameters>) {
+		this.parameters = dialog.context;
+	}
+
+	public close(): void {
+		this.dialog.close('This is a test');
+	}
+
+	public submit(): void {
+		this.dialog.disable();
+		this.progress = 0;
+		interval(500)
+			.pipe(
+				takeWhile(() => this.progress < 100)
+			)
+			.subscribe(() => {
+					if (this.progress < 100) {
+						this.progress = this.progress + 10;
+						this.header.go(this.progress, `${this.progress}%`);
+					}
+				}, () => {
+				},
+				() => {
+					this.dialog.enable();
+				}
+			);
+	}
+}
+
--- projects/systelab-components/src/lib/modal/header/dialog-header.component.ts
+++ projects/systelab-components/src/lib/modal/header/dialog-header.component.ts
@@ -1,7 +1,5 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
-declare var Nanobar: any;
-
 @Component({
 	selector:    'systelab-dialog-header',
 	templateUrl: 'dialog-header.component.html',
@@ -16,28 +14,24 @@ export class DialogHeaderComponent implements AfterViewInit {
 	@Input() withClose = true;
 	@Input() withInfo = false;
 	@Input() withProgressBar = false;
+	@Input() withTextProgressBar = false;
 	@Input() withHome = false;
 	@Input() withMinimize = false;
-
-	@ViewChild('progress', {static: false}) progress: ElementRef;
 
 	@Output() public close = new EventEmitter();
 	@Output() public info = new EventEmitter();
 	@Output() public home = new EventEmitter();
 	@Output() public minimize = new EventEmitter();
 
-	private nanobar: any;
+	public textProgress:string;
+	public progress:number;
+	private on:number;
+	private here:number;
 
 	constructor() {
 	}
 
 	public ngAfterViewInit() {
-		if (this.progress) {
-			const options = {
-				target: this.progress.nativeElement
-			};
-			this.nanobar = new Nanobar(options);
-		}
 	}
 
 	public doClose() {
@@ -56,13 +50,40 @@ export class DialogHeaderComponent implements AfterViewInit {
 		this.minimize.emit();
 	}
 
-	public go(n: number) {
-		if (this.nanobar && this.withProgressBar) {
+	public go(n: number, textProgress?:string) {
+		if (this.withProgressBar || this.withTextProgressBar) {
 			if (n > 100) {
 				n = 100;
 			}
-			this.nanobar.go(n); // size bar 30%
+			if (n >= 0) {
+				this.here = n
+				if (!this.on) {
+					this.on = 1
+					this.move(n)
+				}
+			} else if (this.on) {
+				this.move(n)
+				}
+			this.textProgress = textProgress ?? `${n}%`;
 		}
 	}
+
+	// animation loop
+	private move (width) {
+		const dist = width - this.here
+		if (dist < 0.1 && dist > -0.1) {
+			this.place(this.here)
+			this.on = 0
+		} else {
+			this.place(width - dist / 4)
+		}
+	}
+
+	// set bar width
+	private place (num:number) {
+		this.progress = num;
+	}
+
+
 
 }
