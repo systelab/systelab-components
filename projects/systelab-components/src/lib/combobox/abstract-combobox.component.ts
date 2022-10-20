164def32b741d0f4e55391ae9d51b25443afd160
jgrifell <jgrifell@werfen.com>
2022-10-20 17:19:22 +0200

    show the empty element first in the list

--- projects/systelab-components/src/lib/combobox/abstract-combobox.component.ts
+++ projects/systelab-components/src/lib/combobox/abstract-combobox.component.ts
@@ -69,8 +69,10 @@ export abstract class AbstractComboBox<T> implements AgRendererComponent, OnInit
 	@Input()
 	set values(newValues: Array<any>) {
 		if (newValues) {
+			console.log("values1");
 			if (this.withEmptyValue) {
-				newValues.push({description: '', id: undefined});
+				newValues.unshift({description: '', id: undefined});
+				console.log(newValues);
 			}
 		}
 		this._values = newValues;
