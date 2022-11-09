e0b21634b87f8c88e2a0064e797d2559dfa8316c
Francisco Javier Aguado Joaquín <github@fjaguado.com>
2022-11-09 15:43:05 +0100

    Update NodeJS to version 18
    Update also e2e for using WDIO instead of Protractor

--- projects/showcase/e2e/protractor-update-snapshot.conf.js
+++
@@ -1,7 +0,0 @@
-let config = require('./protractor.conf.js').config;
-
-// If UPDATE_GOLDEN_IMAGES is "1" or "true", blue-harvest will update the reference
-// screenshots instead of comparing against them.
-process.env['UPDATE_GOLDEN_IMAGES'] = 'true';
-
-exports.config = config;
--- projects/showcase/e2e/src/app.po.ts
+++ projects/showcase/e2e/src/app.po.ts
@@ -1,26 +1,17 @@
-import { browser, by, element } from 'protractor';
-import * as path from 'path';
-import { compareScreenshot } from 'snapshot-testing/dist';
+import {BasePage} from "systelab-components-wdio-test";
 
-export class ShowcasePage {
+export class ShowcasePage extends BasePage {
 
-	public navigateTo() {
-		return browser.get('/');
-	}
-
-	public takeScreenshot() {
-		return browser.takeScreenshot();
+	public async navigateTo() {
+		return await browser.url('/');
 	}
 
 	public getNavItem(i: number) {
-		return element(by.id('nav-' + i));
+		return this.byId(`nav-${i}`);
 	}
 
 	public async checkNavigationItem(i: number) {
 		await this.getNavItem(i).click();
-		const data = await this.takeScreenshot();
-		// As in Travis and in retina I get different images, for the moment I will not check the snapshots. Try Jest?
-		//	return await compareScreenshot(data, path.join(__dirname, `snapshots`), `snapshots_${i + 1}.png`);
-		return await true;
+		return true;
 	}
 }
