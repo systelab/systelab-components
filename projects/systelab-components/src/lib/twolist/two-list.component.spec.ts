39962387901be277f48e825791a59f46f6dd091f
Alfons Serra <alfonsoserra@gmail.com>
2023-01-09 14:43:58 +0100

    Set the styles defined in the guideline. (#689)
    * Set the styles defined in the guideline.

--- projects/systelab-components/src/lib/twolist/two-list.component.spec.ts
+++ projects/systelab-components/src/lib/twolist/two-list.component.spec.ts
@@ -89,7 +89,7 @@ describe('Systelab Two list', () => {
 	});
 
 	it('should all elements be visible after clicking button Add All', () => {
-		clickButton(fixture, '.icon-angle-double-right');
+		clickButton(fixture, '#slab-add-all');
 		checkIfListContains(fixture, 'right-list', 'Element 1');
 		checkIfListContains(fixture, 'right-list', 'Element 2');
 		checkIfListContains(fixture, 'right-list', 'Element 3');
@@ -102,7 +102,7 @@ describe('Systelab Two list', () => {
 	});
 
 	it('should none elements be visible after clicking button Remove All', () => {
-		clickButton(fixture, '.icon-angle-double-left');
+		clickButton(fixture, '#slab-remove-all');
 		checkIfListContains(fixture, 'left-list', 'Element 1');
 		checkIfListContains(fixture, 'left-list', 'Element 2');
 		checkIfListContains(fixture, 'left-list', 'Element 3');
