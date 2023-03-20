e62fad7aa05fc122a1a32047b03bd0e32c84918c
tlainez <tlainez@werfen.com>
2023-03-20 17:09:34 +0100

    bugfix-719-image-viewer-minor-fixes

--- projects/showcase/src/app/components/image-viewer/showcase-image-viewer.component.ts
+++ projects/showcase/src/app/components/image-viewer/showcase-image-viewer.component.ts
@@ -12,10 +12,10 @@ export class ShowcaseImageViewerComponent {
 	public imageSrc = 'images/map.jpg';
 	public imageTitle = 'Barcelona Eixample District';
 	public actionButtons: Array<ActionButton> = [
-		{action: 'Action 1', label: 'Action 1', type: ActionButtonType.BUTTON},
-		{action: 'contrast', label: 'High contrast', type: ActionButtonType.TOGGLE_BUTTON},
-		{action: 'green', label: 'Apply green', type: ActionButtonType.TOGGLE_BUTTON},
-		{action: 'red', label: 'Apply red', type: ActionButtonType.TOGGLE_BUTTON}
+		{action: 'Action 1', label: 'Action 1', tooltip: 'Action 1 tooltip', type: ActionButtonType.BUTTON},
+		{action: 'contrast', label: 'High contrast', tooltip: 'High contrast tooltip', type: ActionButtonType.TOGGLE_BUTTON},
+		{action: 'green', label: 'Apply green', tooltip: 'Apply green tooltip', type: ActionButtonType.TOGGLE_BUTTON},
+		{action: 'red', label: 'Apply red', tooltip: 'Apply red tooltip', type: ActionButtonType.TOGGLE_BUTTON}
 	];
 	public imageFilters = `
 		<filter id="red">
