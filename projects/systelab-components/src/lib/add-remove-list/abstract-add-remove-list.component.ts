6b5f4fbbfd47413a3cf7abc94462140d4f425de4
Oscar de Dios <odedios@werfen.com>
2023-09-06 10:09:53 +0200

    Feature 759 (#760)
    * feature-759 adding checkboxes to add and remove

--- projects/systelab-components/src/lib/add-remove-list/abstract-add-remove-list.component.ts
+++ projects/systelab-components/src/lib/add-remove-list/abstract-add-remove-list.component.ts
@@ -7,6 +7,8 @@ export abstract class AbstractAddRemoveList<T> extends AbstractSortableListCompo
 	@Input() public override elementsList: Array<T> = [];
 	@Input() public buttonsOnBottom = false;
 	@Input() public isDisabled = false;
+	@Input() public showChecks = false;
+	@Input() public showSelectedRowsInRemoveButton = false;
 
 	constructor() {
 		super();
@@ -28,6 +30,30 @@ export abstract class AbstractAddRemoveList<T> extends AbstractSortableListCompo
 	public preventDefault(event) {
 		event.mouseEvent.preventDefault();
 		return false;
+	}
+
+	public getSelectedElements(): string {
+		if (this.showSelectedRowsInRemoveButton) {
+			const selectedElements = this.elementsList.filter(element=> element[this.getSelectionField(element)]).length;
+			return selectedElements > 0 ? `(${selectedElements})` : '';
+		} else {
+			return '';
+		}
+	}
+
+	public override selectElement(element: T, ev: KeyboardEvent) {
+		if (!this.showChecks) {
+			if (!ev.ctrlKey) {
+				this.elementsList.forEach(elementInList => {
+					elementInList[this.getSelectionField(element)] = false;
+				});
+			}
+			element[this.getSelectionField(element)] = !element[this.getSelectionField(element)];
+		}
+	}
+
+	public selectCheckbox(element: T) {
+		element[this.getSelectionField(element)] = !element[this.getSelectionField(element)];
 	}
 
 	public abstract override getDescriptionField(element: T): string;
