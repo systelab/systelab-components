d78be9eb119ce1e721caf699ad594c9b2850b5cc
Francisco Javier Aguado Joaquín <github@fjaguado.com>
2026-01-13 17:26:11 +0100

    Upgrade to Angular 21

--- projects/showcase/src/main.ts
+++ projects/showcase/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
@@ -8,4 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(ShowcaseModule);
+platformBrowserDynamic().bootstrapModule(ShowcaseModule, { applicationProviders: [provideZoneChangeDetection()], });
